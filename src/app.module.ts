9c3d7243b6c0e547bc3ba000b2f72bb8c29f68b9
donghunee <ehdgns1766@naver.com>
2022-11-06 20:48:46 +0900

    [update] port

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,7 +17,7 @@ import { AuthModule } from './auth/auth.module';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOSTNAME,
-      port: 5432,
+      port: Number(process.env.DB_PORT),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
