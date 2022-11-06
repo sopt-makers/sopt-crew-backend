389dd60db88dc29a1a1d6d40b8da06d660ea6e7c
donghunee <ehdgns1766@naver.com>
2022-11-06 21:00:38 +0900

    [update]

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,7 +16,7 @@ import { AuthModule } from './auth/auth.module';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOSTNAME,
+      host: process.env.DB_HOST,
       port: Number(process.env.DB_PORT),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
