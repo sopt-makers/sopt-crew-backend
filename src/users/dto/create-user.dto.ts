f7bc018e46898c1417478d9ed90d9d307c4d6067
donghunee <ehdgns1766@naver.com>
2023-03-05 22:34:13 +0900

    feat: create user dto 생성

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,28 @@
+import { IsString, IsNumber } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateUserDto {
+  @ApiProperty({
+    example: 3,
+    description: 'sopt org unique id',
+    required: true,
+  })
+  @IsNumber()
+  readonly orgId: number;
+
+  @ApiProperty({
+    example: '이동이동',
+    description: '이름 검색',
+    required: true,
+  })
+  @IsString()
+  readonly name: string;
+
+  @ApiProperty({
+    example: 'http://s3',
+    description: '프로필 이미지',
+    required: true,
+  })
+  @IsString()
+  readonly profileImage: string;
+}
