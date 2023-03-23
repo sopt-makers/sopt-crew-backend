d270a3e584ad1a771f509ba923fc1438abdfb7e4
YeongWoooo <rdd9223@gmail.com>
2023-03-23 23:20:39 +0900

    docs: swagger에 dev용 default 토큰 변경

--- server/src/auth/dto/auth-token.dto.ts
+++ server/src/auth/dto/auth-token.dto.ts
@@ -4,8 +4,9 @@ import { IsNotEmpty, IsString } from 'class-validator';
 export class AuthTokenDTO {
   @ApiProperty({
     example:
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoibGUzZTMiLCJ1c2VySWQiOjIsImlhdCI6MTY2ODQzMzg0OSwiZXhwIjoxNzA0NDMzODQ5fQ.Xd69DWu7Z5FYECeSWcReGUoflq2UOiEOpVa_vdjRJEY',
-    description: 'playgroud authToken',
+      'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxOCIsImV4cCI6MTY3OTYwOTk3OH0.9D_Tc14J3S0VDmQgT5lUJ5i3KJZob3NKVmSS3fPjHAo',
+    description:
+      '영우의 playgroud authToken (남용하지 마시오. dev환경에서만 동작)',
     required: true,
   })
   @IsString()
