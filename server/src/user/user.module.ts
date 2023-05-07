6b9f86b8fbb93f4c101c5c08e31df87d90dc4f1a
YeongWoooo <rdd9223@gmail.com>
2023-05-07 14:50:08 +0900

    [#24] feat: 유저 정보 가져오는 API 구현
    - profileImage에 null이 될 수 있는데 타입이 추가가 안되었던 경우 수정
    - jest 설정 파일 외부로 새로 생성
    - 테스트코드 작성
    - 새로운 user엔드포인트 v1 생성

--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UserV0Module } from './v0/user-v0.module';
+import { UserV1Module } from './v1/user-v1.module';
 
 @Module({
-  imports: [UserV0Module],
+  imports: [UserV0Module, UserV1Module],
 })
 export class UserModule {}
---
+++ server/src/user/v1/user-v1.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { UserV1Service } from './user-v1.service';
+import { ApiTags, ApiOperation, ApiBearerAuth } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from '../../entity/user/user.entity';
+import { GetUser } from 'src/common/decorator/get-user.decorator';
+import { UserV1GetUserOwnProfileResponseDto } from './dto/user-v1-get-user-own-profile/user-v1-get-user-own-profile-response.dto';
+
+@ApiTags('사용자')
+@Controller('users/v1')
+export class UserV1Controller {
+  constructor(private usersV1Service: UserV1Service) {}
+
+  @ApiOperation({
+    summary: '유저 본인 프로필 조회',
+    description: '유저 본인 프로필 조회',
+  })
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('jwt'))
+  @Get('/profile/me')
+  async getUserOwnProfile(
+    @GetUser() user: User,
+  ): Promise<UserV1GetUserOwnProfileResponseDto> {
+    return this.usersV1Service.getUserOwnProfile(user);
+  }
+}
