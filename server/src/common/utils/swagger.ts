7200c193fa01b96fe7b53dd7967c2d5cb56236e0
YeongWoooo <rdd9223@gmail.com>
2023-10-03 00:54:09 +0900

    [#77] v0.5.2

--- server/src/common/utils/swagger.ts
+++ server/src/common/utils/swagger.ts
@@ -12,7 +12,7 @@ export function setupSwagger(app: INestApplication): void {
   const options = new DocumentBuilder()
     .setTitle('Crew API 문서')
     .setDescription('Crew API 문서')
-    .setVersion('0.5.1')
+    .setVersion('0.5.2')
     .addBearerAuth()
     .build();
 
