89e274a727e3ba2a71e28441b48d71f19553835b
YeongWoooo <rdd9223@gmail.com>
2023-10-03 00:46:06 +0900

    [#75] 모임 게시글 삭제시 해당 정보만 삭제하도록 변경

--- server/src/meeting/v0/meeting-v0.controller.ts
+++ server/src/meeting/v0/meeting-v0.controller.ts
@@ -38,7 +38,6 @@ import { MeetingV0GetApplyListByMeetingResponseDto } from './dto/get-apply-list-
 import { MeetingV0GetAllMeetingsResponseDto } from './dto/get-all-meetings/meeting-v0-get-all-meetings-response.dto';
 import { GetUser } from 'src/common/decorator/get-user.decorator';
 import { ApiOkResponseCommon } from 'src/common/decorator/api-ok-response-common.decorator';
-import { Apply } from 'src/entity/apply/apply.entity';
 
 @ApiTags('모임')
 @Controller('meeting')
--- server/src/meeting/v1/meeting-v1.controller.ts
+++ server/src/meeting/v1/meeting-v1.controller.ts
@@ -2,7 +2,6 @@ import {
   Body,
   Controller,
   Get,
-  HttpCode,
   HttpStatus,
   Param,
   ParseIntPipe,
