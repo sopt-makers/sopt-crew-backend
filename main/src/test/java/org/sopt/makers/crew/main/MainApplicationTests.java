c0e2db4cdedcdb63aac15fbb4d6105b79b06bcd4
YeongWoooo <rdd9223@gmail.com>
2023-10-16 00:03:48 +0900

    [#39] env: 스프링 초기 프로젝트 생성

---
+++ main/src/test/java/org/sopt/makers/crew/main/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sopt.makers.crew.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
