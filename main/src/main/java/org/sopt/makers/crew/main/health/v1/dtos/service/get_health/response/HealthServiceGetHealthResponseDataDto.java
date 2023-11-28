9f156ceb19b6d0343383d241849d3a67ac838655
yeseul106 <20191037@sungshin.ac.kr>
2023-11-29 04:28:19 +0900

    [#86] DTO 및 컨트롤러 클래스 생성자 전략 리팩토링

--- main/src/main/java/org/sopt/makers/crew/main/health/v1/dtos/service/get_health/response/HealthServiceGetHealthResponseDataDto.java
+++ main/src/main/java/org/sopt/makers/crew/main/health/v1/dtos/service/get_health/response/HealthServiceGetHealthResponseDataDto.java
@@ -1,13 +1,26 @@
 package org.sopt.makers.crew.main.health.v1.dtos.service.get_health.response;
 
+import static lombok.AccessLevel.PRIVATE;
+
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@Setter
+@NoArgsConstructor(access = PRIVATE)
 public class HealthServiceGetHealthResponseDataDto {
   private String status;
   private HealthServiceGetHealthResponseDataInfoDto info;
   private HealthServiceGetHealthResponseDataInfoDto error;
   private HealthServiceGetHealthResponseDataInfoDto details;
+
+  @Builder
+  public HealthServiceGetHealthResponseDataDto(String status, HealthServiceGetHealthResponseDataInfoDto info,
+                                               HealthServiceGetHealthResponseDataInfoDto error,
+                                               HealthServiceGetHealthResponseDataInfoDto details) {
+    this.status = status;
+    this.info = info;
+    this.error = error;
+    this.details = details;
+  }
 }
