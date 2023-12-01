b4f5d7f8cfd415a975f26e65861b9a0f99ab817f
yeseul106 <20191037@sungshin.ac.kr>
2023-12-02 02:46:14 +0900

    [#86] 대략적인 apiResponse 객체 구조 수정

--- main/src/main/java/org/sopt/makers/crew/main/common/advice/ControllerExceptionAdvice.java
+++ main/src/main/java/org/sopt/makers/crew/main/common/advice/ControllerExceptionAdvice.java
@@ -1,13 +1,10 @@
 package org.sopt.makers.crew.main.common.advice;
 
-import java.util.Objects;
 import org.sopt.makers.crew.main.common.exception.BaseException;
 import org.sopt.makers.crew.main.common.response.ApiResponse;
 import org.sopt.makers.crew.main.common.response.ErrorStatus;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -18,27 +15,19 @@ public class ControllerExceptionAdvice {
     @ExceptionHandler(BaseException.class)
     public ResponseEntity<ApiResponse> handleGlobalException(BaseException ex) {
         return ResponseEntity.status(ex.getStatusCode())
-                .body(ApiResponse.fail(ex.getStatusCode(), ex.getResponseMessage()));
+                .body(ApiResponse.fail(ex.getErrorCode()));
     }
 
     @ExceptionHandler(MissingServletRequestParameterException.class)
     public ResponseEntity<ApiResponse> handleMissingParameter(MissingServletRequestParameterException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(ApiResponse.fail(HttpStatus.BAD_REQUEST.value(),
-                        ErrorStatus.VALIDATION_REQUEST_MISSING_EXCEPTION.getMessage()));
+                .body(ApiResponse.fail(ErrorStatus.VALIDATION_REQUEST_MISSING_EXCEPTION.getErrorCode()));
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ApiResponse>  handleIllegalArgument(IllegalArgumentException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(ApiResponse.fail(HttpStatus.BAD_REQUEST.value(), ex.getMessage()));
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    protected ResponseEntity<ApiResponse> handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
-        FieldError fieldError = Objects.requireNonNull(e.getFieldError());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(ApiResponse.fail(HttpStatus.BAD_REQUEST.value(),String.format("%s. (%s)", fieldError.getDefaultMessage(), fieldError.getField())));
+                .body(ApiResponse.fail(ex.getMessage()));
     }
 
 }
