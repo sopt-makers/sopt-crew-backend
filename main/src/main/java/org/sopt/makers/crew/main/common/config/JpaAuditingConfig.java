2a7951b281ef85837f809667e37d7c509d436497
yeseul106 <20191037@sungshin.ac.kr>
2023-12-02 01:01:58 +0900

    [#86] JpaAuditing 관련 config 파일 추가

---
+++ main/src/main/java/org/sopt/makers/crew/main/common/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package org.sopt.makers.crew.main.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {
+}
