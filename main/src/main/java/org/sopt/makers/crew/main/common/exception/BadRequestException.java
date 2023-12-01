371ef9a8e9dea1270f4ac2ad0a0617f4cfa2dacf
yeseul106 <20191037@sungshin.ac.kr>
2023-12-02 01:00:47 +0900

    [#86] Http Status code에 따른 예외 처리 추가

---
+++ main/src/main/java/org/sopt/makers/crew/main/common/exception/BadRequestException.java
@@ -0,0 +1,14 @@
+package org.sopt.makers.crew.main.common.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class BadRequestException extends BaseException {
+
+    public BadRequestException() {
+        super(HttpStatus.BAD_REQUEST);
+    }
+
+    public BadRequestException(String responseMessage) {
+        super(HttpStatus.BAD_REQUEST, responseMessage);
+    }
+}
