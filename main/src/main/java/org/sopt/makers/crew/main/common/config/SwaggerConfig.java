e7156ced13d065e1d80740d1b6971eeaf00ae79d
yeseul106 <20191037@sungshin.ac.kr>
2023-12-05 17:05:08 +0900

    [#89] add: Swagger 관련 config 추가

---
+++ main/src/main/java/org/sopt/makers/crew/main/common/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package org.sopt.makers.crew.main.common.config;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SecurityScheme(
+    name = "bearer",
+    type = SecuritySchemeType.HTTP,
+    bearerFormat = "JWT",
+    scheme = "bearer"
+)
+public class SwaggerConfig {
+
+  @Bean
+  public OpenAPI customOpenAPI() {
+    return new OpenAPI()
+        .info(new Info()
+            .title("Crew API 문서")
+            .version("0.5.3")
+            .description("Crew API 문서"));
+  }
+}
