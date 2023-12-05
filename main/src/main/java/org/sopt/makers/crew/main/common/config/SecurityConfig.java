1f1cfa16bf79a967504cfca9ef44b5aa4f64a8b3
yeseul106 <20191037@sungshin.ac.kr>
2023-12-05 17:04:36 +0900

    [#89] add: Spring Security 세팅

---
+++ main/src/main/java/org/sopt/makers/crew/main/common/config/SecurityConfig.java
@@ -0,0 +1,61 @@
+package org.sopt.makers.crew.main.common.config;
+
+import lombok.RequiredArgsConstructor;
+import org.sopt.makers.crew.main.common.config.jwt.JwtAuthenticationEntryPoint;
+import org.sopt.makers.crew.main.common.config.jwt.JwtAuthenticationFilter;
+import org.sopt.makers.crew.main.common.config.jwt.JwtTokenProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+
+  private final JwtTokenProvider jwtTokenProvider;
+  private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+  private static final String[] SWAGGER_URL = {
+      "/swagger-resources/**",
+      "/favicon.ico",
+      "/api-docs/**",
+      "/swagger-ui/**",
+      "/swagger-ui.html",
+      "/swagger-ui/index.html",
+      "/docs/swagger-ui/index.html",
+      "/swagger-ui/swagger-ui.css",
+  };
+
+  private static final String[] AUTH_WHITELIST = {
+      "/health"
+  };
+
+  @Bean
+  @Profile("dev")
+  SecurityFilterChain prodSecurityFilterChain(HttpSecurity http) throws Exception {
+    return http.csrf((csrfConfig) ->
+            csrfConfig.disable()
+        )
+        .sessionManagement((sessionManagement) ->
+            sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        )
+        .authorizeHttpRequests(
+            authorize -> authorize.requestMatchers(AUTH_WHITELIST).permitAll()
+                .requestMatchers(SWAGGER_URL).permitAll()
+                .anyRequest().authenticated()
+        )
+        .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider, jwtAuthenticationEntryPoint),
+            UsernamePasswordAuthenticationFilter.class)
+        .exceptionHandling(exceptionHandling ->
+            exceptionHandling.authenticationEntryPoint(jwtAuthenticationEntryPoint)
+        )
+        .build();
+  }
+
+}
--- main/src/main/java/org/sopt/makers/crew/main/common/response/ErrorStatus.java
+++ main/src/main/java/org/sopt/makers/crew/main/common/response/ErrorStatus.java
@@ -8,17 +8,22 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 
 public enum ErrorStatus {
-    /**
-     * 400 BAD_REQUEST
-     */
-    VALIDATION_EXCEPTION("CR-001"), // errorCode는 예시, 추후 변경 예정 -> 잘못된 요청입니다.
-    VALIDATION_REQUEST_MISSING_EXCEPTION("요청값이 입력되지 않았습니다."),
+  /**
+   * 400 BAD_REQUEST
+   */
+  VALIDATION_EXCEPTION("CR-001"), // errorCode는 예시, 추후 변경 예정 -> 잘못된 요청입니다.
+  VALIDATION_REQUEST_MISSING_EXCEPTION("요청값이 입력되지 않았습니다."),
 
-    /**
-     * 500 SERVER_ERROR
-     */
-    INTERNAL_SERVER_ERROR("예상치 못한 서버 에러가 발생했습니다.");
+  /**
+   * 401 UNAUTHORIZED
+   */
+  UNAUTHORIZED_TOKEN("유효하지 않은 토큰입니다."),
 
-    private final String errorCode;
+  /**
+   * 500 SERVER_ERROR
+   */
+  INTERNAL_SERVER_ERROR("예상치 못한 서버 에러가 발생했습니다.");
+
+  private final String errorCode;
 
 }
