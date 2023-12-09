a7e18ce1124659e655308a63548e0f5231d30dea
yeseul106 <20191037@sungshin.ac.kr>
2023-12-10 02:03:56 +0900

    [#101] fix: swagger에서 Authorize 정상 작동하도록 수정

--- main/src/main/java/org/sopt/makers/crew/main/common/config/SwaggerConfig.java
+++ main/src/main/java/org/sopt/makers/crew/main/common/config/SwaggerConfig.java
@@ -1,27 +1,39 @@
 package org.sopt.makers.crew.main.common.config;
 
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import static io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP;
+
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@SecurityScheme(
-    name = "bearer",
-    type = SecuritySchemeType.HTTP,
-    bearerFormat = "JWT",
-    scheme = "bearer"
-)
 public class SwaggerConfig {
 
   @Bean
-  public OpenAPI customOpenAPI() {
+  public OpenAPI openAPI() {
+
+    Info info = new Info()
+        .title("Crew API 문서")
+        .version("0.5.3")
+        .description("Crew API 문서");
+
+    String jwtSchemeName = "JWT Authorization";
+    SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+
+    Components components = new Components()
+        .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+            .name(jwtSchemeName)
+            .type(HTTP) // HTTP 방식
+            .scheme("bearer")
+            .bearerFormat("JWT"));
+
     return new OpenAPI()
-        .info(new Info()
-            .title("Crew API 문서")
-            .version("0.5.3")
-            .description("Crew API 문서"));
+        .info(info)
+        .addSecurityItem(securityRequirement)
+        .components(components);
   }
 }
