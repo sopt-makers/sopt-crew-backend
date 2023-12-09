93a82a28d89ba13c1f38e2c64185badc965abb56
yeseul106 <20191037@sungshin.ac.kr>
2023-12-10 02:43:20 +0900

    [#101] fix: deprecated된 security cors 설정 코드 수정

--- main/src/main/java/org/sopt/makers/crew/main/common/config/SecurityConfig.java
+++ main/src/main/java/org/sopt/makers/crew/main/common/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package org.sopt.makers.crew.main.common.config;
 
+import java.util.Arrays;
 import lombok.RequiredArgsConstructor;
 import org.sopt.makers.crew.main.common.config.jwt.JwtAuthenticationEntryPoint;
 import org.sopt.makers.crew.main.common.config.jwt.JwtAuthenticationFilter;
@@ -7,11 +8,15 @@ import org.sopt.makers.crew.main.common.config.jwt.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @RequiredArgsConstructor
@@ -39,9 +44,10 @@ public class SecurityConfig {
   @Bean
   @Profile("dev")
   SecurityFilterChain devSecurityFilterChain(HttpSecurity http) throws Exception {
-    return http.csrf((csrfConfig) ->
+    http.csrf((csrfConfig) ->
             csrfConfig.disable()
         )
+        .cors(Customizer.withDefaults())
         .sessionManagement((sessionManagement) ->
             sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         )
@@ -50,12 +56,27 @@ public class SecurityConfig {
                 .requestMatchers(SWAGGER_URL).permitAll()
                 .anyRequest().authenticated()
         )
-        .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider, jwtAuthenticationEntryPoint),
+        .addFilterBefore(
+            new JwtAuthenticationFilter(jwtTokenProvider, jwtAuthenticationEntryPoint),
             UsernamePasswordAuthenticationFilter.class)
         .exceptionHandling(exceptionHandling ->
             exceptionHandling.authenticationEntryPoint(jwtAuthenticationEntryPoint)
-        )
-        .build();
+        );
+    return http.build();
+  }
+
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(
+        Arrays.asList("https://playground.sopt.org/", "http://localhost:3000"));
+    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE", "OPTIONS"));
+    configuration.addAllowedHeader("*");
+    configuration.setAllowCredentials(false);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 
 }
