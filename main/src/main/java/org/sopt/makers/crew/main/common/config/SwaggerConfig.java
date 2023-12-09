dbec1428aa6f36cba58a6094cf2bace1ae28a41a
YeongWoooo <rdd9223@gmail.com>
2023-12-10 04:20:28 +0900

    [#104] v1.0.0

--- main/src/main/java/org/sopt/makers/crew/main/common/config/SwaggerConfig.java
+++ main/src/main/java/org/sopt/makers/crew/main/common/config/SwaggerConfig.java
@@ -16,27 +16,27 @@ import org.springframework.context.annotation.Configuration;
 @OpenAPIDefinition(servers = { @Server(url = "/", description = "Generated Server URL") })
 public class SwaggerConfig {
 
-    @Bean
-    public OpenAPI openAPI() {
+        @Bean
+        public OpenAPI openAPI() {
 
-        Info info = new Info()
-                .title("Crew API 문서")
-                .version("0.5.3")
-                .description("Crew API 문서");
+                Info info = new Info()
+                                .title("Crew API 문서")
+                                .version("1.0.0")
+                                .description("Crew API 문서");
 
-        String jwtSchemeName = "JWT Authorization";
-        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+                String jwtSchemeName = "JWT Authorization";
+                SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
 
-        Components components = new Components()
-                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
-                        .name(jwtSchemeName)
-                        .type(HTTP) // HTTP 방식
-                        .scheme("bearer")
-                        .bearerFormat("JWT"));
+                Components components = new Components()
+                                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                                                .name(jwtSchemeName)
+                                                .type(HTTP) // HTTP 방식
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT"));
 
-        return new OpenAPI()
-                .info(info)
-                .addSecurityItem(securityRequirement)
-                .components(components);
-    }
+                return new OpenAPI()
+                                .info(info)
+                                .addSecurityItem(securityRequirement)
+                                .components(components);
+        }
 }
