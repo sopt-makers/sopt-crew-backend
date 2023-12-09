79728b84acd806e908d012ecaebcfe4b3c81a68f
YeongWoooo <rdd9223@gmail.com>
2023-12-09 19:41:58 +0900

    [#94] docs: spring swagger 요청시 https로 요청하게 변경
    - cors 에러때문에 http 요청이 default지만 https로 요청하도록 변경

--- main/src/main/java/org/sopt/makers/crew/main/common/config/SwaggerConfig.java
+++ main/src/main/java/org/sopt/makers/crew/main/common/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package org.sopt.makers.crew.main.common.config;
 
 import static io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -11,29 +13,30 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Generated Server URL") })
 public class SwaggerConfig {
 
-  @Bean
-  public OpenAPI openAPI() {
+    @Bean
+    public OpenAPI openAPI() {
 
-    Info info = new Info()
-        .title("Crew API 문서")
-        .version("0.5.3")
-        .description("Crew API 문서");
+        Info info = new Info()
+                .title("Crew API 문서")
+                .version("0.5.3")
+                .description("Crew API 문서");
 
-    String jwtSchemeName = "JWT Authorization";
-    SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+        String jwtSchemeName = "JWT Authorization";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
 
-    Components components = new Components()
-        .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
-            .name(jwtSchemeName)
-            .type(HTTP) // HTTP 방식
-            .scheme("bearer")
-            .bearerFormat("JWT"));
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(HTTP) // HTTP 방식
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
 
-    return new OpenAPI()
-        .info(info)
-        .addSecurityItem(securityRequirement)
-        .components(components);
-  }
+        return new OpenAPI()
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
 }
