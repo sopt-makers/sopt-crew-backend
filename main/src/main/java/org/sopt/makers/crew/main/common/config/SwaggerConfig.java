53fb5785e9517ff51b5602a4327d1a2911b897a3
YeongWoooo <rdd9223@gmail.com>
2023-12-10 04:46:22 +0900

    [#109] v1.0.1

--- main/src/main/java/org/sopt/makers/crew/main/common/config/SwaggerConfig.java
+++ main/src/main/java/org/sopt/makers/crew/main/common/config/SwaggerConfig.java
@@ -21,7 +21,7 @@ public class SwaggerConfig {
 
                 Info info = new Info()
                                 .title("Crew API 문서")
-                                .version("1.0.0")
+                                .version("1.0.1")
                                 .description("Crew API 문서");
 
                 String jwtSchemeName = "JWT Authorization";
--- server/src/common/utils/swagger.ts
+++ server/src/common/utils/swagger.ts
@@ -12,7 +12,7 @@ export function setupSwagger(app: INestApplication): void {
   const options = new DocumentBuilder()
     .setTitle('Crew API 문서')
     .setDescription('Crew API 문서')
-    .setVersion('1.0.0')
+    .setVersion('1.0.1')
     .addBearerAuth()
     .build();
 
