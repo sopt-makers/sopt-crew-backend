0081c1dd036d3df63f441971a43a82b9d17d00a6
yeseul106 <20191037@sungshin.ac.kr>
2024-01-25 01:23:36 +0900

    [FIX] #122 user repository에 누락된 조회 쿼리 코드 추가

--- main/src/main/java/org/sopt/makers/crew/main/entity/user/UserRepository.java
+++ main/src/main/java/org/sopt/makers/crew/main/entity/user/UserRepository.java
@@ -1,12 +1,20 @@
 package org.sopt.makers.crew.main.entity.user;
 
+import java.util.Optional;
 import org.sopt.makers.crew.main.common.exception.UnAuthorizedException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
+  Optional<User> findByOrgId(Integer orgId);
+
   default User findByIdOrThrow(Integer userId) {
     return findById(userId)
+        .orElseThrow(() -> new UnAuthorizedException());
+  }
+
+  default User findByOrgIdOrThrow(Integer orgUserId) {
+    return findByOrgId(orgUserId)
         .orElseThrow(() -> new UnAuthorizedException());
   }
 }
