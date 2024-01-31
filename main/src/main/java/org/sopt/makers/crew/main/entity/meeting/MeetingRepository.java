e0ecaf3f59d84bea46ace5afc477d6e81d9d9814
yeseul106 <20191037@sungshin.ac.kr>
2024-01-31 22:34:02 +0900

    [#119] feat: 모임 게시글 작성 API 마이그레이션

--- main/src/main/java/org/sopt/makers/crew/main/entity/meeting/MeetingRepository.java
+++ main/src/main/java/org/sopt/makers/crew/main/entity/meeting/MeetingRepository.java
@@ -1,9 +1,18 @@
 package org.sopt.makers.crew.main.entity.meeting;
 
 import java.util.List;
+import java.util.Optional;
+import org.sopt.makers.crew.main.common.exception.BadRequestException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MeetingRepository extends JpaRepository<Meeting, Integer> {
 
   List<Meeting> findAllByUserId(int userId);
+
+  Optional<Meeting> findById(Integer meetingId);
+
+  default Meeting findByIdOrThrow(Integer meetingId) {
+    return findById(meetingId)
+        .orElseThrow(() -> new BadRequestException("모임이 없습니다."));
+  }
 }
---
+++ main/src/main/java/org/sopt/makers/crew/main/post/v2/PostV2Controller.java
@@ -0,0 +1,44 @@
+package org.sopt.makers.crew.main.post.v2;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import java.security.Principal;
+import lombok.RequiredArgsConstructor;
+import org.sopt.makers.crew.main.common.util.UserUtil;
+import org.sopt.makers.crew.main.post.v2.dto.request.PostV2CreatePostBodyDto;
+import org.sopt.makers.crew.main.post.v2.dto.response.PostV2CreatePostResponseDto;
+import org.sopt.makers.crew.main.post.v2.service.PostV2Service;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/post/v2")
+@RequiredArgsConstructor
+@Tag(name = "모임")
+public class PostV2Controller {
+
+  private final PostV2Service postV2Service;
+
+  @Operation(summary = "모임 게시글 작성")
+  @PostMapping()
+  @ResponseStatus(HttpStatus.CREATED)
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "201", description = "성공"),
+      @ApiResponse(responseCode = "400", description = "모임이 없습니다.", content = @Content),
+      @ApiResponse(responseCode = "403", description = "권한이 없습니다.", content = @Content),
+  })
+  public ResponseEntity<PostV2CreatePostResponseDto> createPost(
+      @Valid @RequestBody PostV2CreatePostBodyDto requestBody, Principal principal) {
+    Integer userId = UserUtil.getUserId(principal);
+    return ResponseEntity.ok(postV2Service.createPost(requestBody, userId));
+  }
+}
