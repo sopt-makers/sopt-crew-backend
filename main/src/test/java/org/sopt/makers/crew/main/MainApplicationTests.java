d86bbda1de1e02d11018e01257371b896331da67
mikekks <thdalsrb79@naver.com>
2024-03-18 20:20:48 +0900

    [ADD] 테스트 코드 환경 설정

--- main/src/test/java/org/sopt/makers/crew/main/MainApplicationTests.java
+++ main/src/test/java/org/sopt/makers/crew/main/MainApplicationTests.java
@@ -1,12 +1,15 @@
 package org.sopt.makers.crew.main;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MainApplicationTests {
 
-	// @Test
-	// void contextLoads() {
-	// }
+    @Test
+	 void contextLoads() {
+	 }
 
 }
