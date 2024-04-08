250673a3193d105533b945c10b94ffc9b168c2eb
mikekks <thdalsrb79@naver.com>
2024-04-08 20:11:16 +0900

    [DEL] schema.sql 의존하는 코드 제거

--- main/src/test/java/org/sopt/makers/crew/main/entity/user/UserRepositoryTest.java
+++ main/src/test/java/org/sopt/makers/crew/main/entity/user/UserRepositoryTest.java
@@ -6,13 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.jdbc.Sql;
 
 @DataJpaTest
 @ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@Sql(scripts = "classpath:schema.sql")
 public class UserRepositoryTest {
 
     @Autowired
