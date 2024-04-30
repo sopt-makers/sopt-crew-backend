5ea300eb4a39d38ca122e60889932a7b3234bafb
Mingyu Song <100754581+mikekks@users.noreply.github.com>
2024-04-30 13:38:47 +0900

    [FEAT] 모임생성 API 마이그레이션 (#170)
    * [ADD] MapStruct 추가
    
    * [ADD] 전역 상수 추가
    
    * [ADD] 세미나 enum 추가
    
    * [ADD] 모임 매퍼 추가
    
    * [FEAT] 스웨거 관련 메타 데이터 인터페이스로 분리
    
    * [FEAT] 모임생성 컨트롤러 로직 구현
    
    * [ADD] 요청, 응답 Dto 추가
    
    * [FEAT] 모임생성 서비스 로직 구현
    
    * [CHORE] 에러 코드 수정

---
+++ main/src/main/java/org/sopt/makers/crew/main/meeting/v2/dto/response/MeetingV2CreateMeetingResponseDto.java
@@ -0,0 +1,10 @@
+package org.sopt.makers.crew.main.meeting.v2.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(staticName = "of")
+public class MeetingV2CreateMeetingResponseDto {
+    private Integer meetingId;
+}
--- server/src/entity/meeting/enum/meeting-category.enum.ts
+++ server/src/entity/meeting/enum/meeting-category.enum.ts
@@ -3,4 +3,5 @@ export enum MeetingCategory {
   LECTURE = '강연',
   LIGHTNING = '번개',
   EVENT = '행사',
+  SEMINAR = '세미나',
 }
