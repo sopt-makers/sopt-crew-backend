799b41ed5d5beb2fa4e0b480a46c6a7e08065c7e
yeseul106 <20191037@sungshin.ac.kr>
2024-05-17 15:34:24 +0900

    [ADD] UserPart와 MeetingJoinablePart Mapper Util 추가

--- main/src/main/java/org/sopt/makers/crew/main/common/util/UserPartUtil.java
+++ main/src/main/java/org/sopt/makers/crew/main/common/util/UserPartUtil.java
@@ -1,4 +1,35 @@
 package org.sopt.makers.crew.main.common.util;
 
+import lombok.RequiredArgsConstructor;
+import org.sopt.makers.crew.main.entity.meeting.enums.MeetingJoinablePart;
+import org.sopt.makers.crew.main.entity.user.enums.UserPart;
+
+@RequiredArgsConstructor
 public class UserPartUtil {
+
+    public static MeetingJoinablePart getMeetingJoinablePart(UserPart userPart) {
+        switch (userPart) {
+            case PM:
+            case PM_LEADER:
+                return MeetingJoinablePart.PM;
+            case DESIGN:
+            case DESIGN_LEADER:
+                return MeetingJoinablePart.DESIGN;
+            case IOS:
+            case IOS_LEADER:
+                return MeetingJoinablePart.IOS;
+            case ANDROID:
+            case ANDROID_LEADER:
+                return MeetingJoinablePart.ANDROID;
+            case SERVER:
+            case SERVER_LEADER:
+                return MeetingJoinablePart.SERVER;
+            case WEB:
+            case WEB_LEADER:
+                return MeetingJoinablePart.WEB;
+            default:
+                // 임원진 등
+                return null;
+        }
+    }
 }
