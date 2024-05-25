3e82a30567df7d6ac01c785c9f2aa6cb7c9c2a42
mikekks <thdalsrb79@naver.com>
2024-05-25 15:45:12 +0900

    [ADD] queryFactory 추가

--- main/src/main/java/org/sopt/makers/crew/main/common/config/JpaAuditingConfig.java
+++ main/src/main/java/org/sopt/makers/crew/main/common/config/JpaAuditingConfig.java
@@ -1,9 +1,20 @@
 package org.sopt.makers.crew.main.common.config;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @Configuration
 public class JpaAuditingConfig {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory queryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
 }
