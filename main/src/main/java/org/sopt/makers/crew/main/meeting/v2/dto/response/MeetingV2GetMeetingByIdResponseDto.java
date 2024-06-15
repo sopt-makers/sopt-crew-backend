0adc7048b526b5a326e740eb7f178981977e400a
mikekks <thdalsrb79@naver.com>
2024-06-15 21:43:46 +0900

    [CHORE] 응답 Dto of 메서드 추가

--- main/src/main/java/org/sopt/makers/crew/main/meeting/v2/dto/response/MeetingV2GetMeetingByIdResponseDto.java
+++ main/src/main/java/org/sopt/makers/crew/main/meeting/v2/dto/response/MeetingV2GetMeetingByIdResponseDto.java
@@ -3,44 +3,59 @@ package org.sopt.makers.crew.main.meeting.v2.dto.response;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import java.util.List;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.sopt.makers.crew.main.entity.meeting.Meeting;
 import org.sopt.makers.crew.main.entity.meeting.enums.EnMeetingStatus;
 import org.sopt.makers.crew.main.entity.meeting.enums.MeetingJoinablePart;
 import org.sopt.makers.crew.main.entity.meeting.vo.ImageUrlVO;
 
 @Getter
-@AllArgsConstructor(staticName = "of")
+@RequiredArgsConstructor
 @Schema(description = "모임 상세 조회 dto")
 public class MeetingV2GetMeetingByIdResponseDto {
-    Integer meetingId;
-    String title;
-    String category;
-    List<ImageUrlVO> imageURLs;
-    LocalDateTime applyStartDate;
-    LocalDateTime applyEndDate;
-    int capacity;
-    String desc;
-    String processDesc;
-    LocalDateTime activityStartDate;
-    LocalDateTime activityEndDate;
-    String leaderDesc;
-    String targetDesc;
-    String note;
-    Boolean isMentorNeeded;
-    Boolean canJoinOnlyActiveGeneration;
-    Integer createdGeneration;
-    Integer targetActiveGeneration;
-    MeetingJoinablePart[] joinableParts;
+    private final Integer meetingId;
+    private final String title;
+    private final String category;
+    private final List<ImageUrlVO> imageURLs;
+    private final LocalDateTime startDate;
+    private final LocalDateTime endDate;
+    private final int capacity;
+    private final String desc;
+    private final String processDesc;
+    private final LocalDateTime activityStartDate;
+    private final LocalDateTime activityEndDate;
+    private final String leaderDesc;
+    private final String targetDesc;
+    private final String note;
+    private final Boolean isMentorNeeded;
+    private final Boolean canJoinOnlyActiveGeneration;
+    private final Integer createdGeneration;
+    private final Integer targetActiveGeneration;
+    private final MeetingJoinablePart[] joinableParts;
 
-    EnMeetingStatus status;
-    Integer approvedApplyCount;
-    Boolean host;
-    Boolean apply;
-    Boolean approved;
+    private final EnMeetingStatus status;
+    private final long approvedApplyCount;
+    private final Boolean host;
+    private final Boolean apply;
+    private final Boolean approved;
 
-    MeetingCreatorDto meetingCreator;
+    private final MeetingCreatorDto meetingCreator;
 
-    List<ApplyInfoDto> appliedInfo;
+    private final List<SimpleApplyInfoResponseDto> appliedInfo;
 
+    public static MeetingV2GetMeetingByIdResponseDto of(Meeting meeting, long approvedCount, Boolean isHost, Boolean isApply,
+                                                        Boolean isApproved, MeetingCreatorDto meetingCreator,
+                                                        List<SimpleApplyInfoResponseDto> appliedInfo, LocalDateTime now) {
+
+        EnMeetingStatus meetingStatus = meeting.getMeetingStatus(now);
+
+        return new MeetingV2GetMeetingByIdResponseDto(meeting.getId(), meeting.getTitle(),
+                meeting.getCategory().getValue(), meeting.getImageURL(), meeting.getStartDate(), meeting.getEndDate(),
+                meeting.getCapacity(), meeting.getDesc(), meeting.getProcessDesc(), meeting.getMStartDate(),
+                meeting.getMEndDate(), meeting.getLeaderDesc(), meeting.getTargetDesc(), meeting.getNote(),
+                meeting.getIsMentorNeeded(), meeting.getCanJoinOnlyActiveGeneration(), meeting.getCreatedGeneration(),
+                meeting.getTargetActiveGeneration(), meeting.getJoinableParts(), meetingStatus,
+                approvedCount, isHost, isApply, isApproved, meetingCreator, appliedInfo);
+    }
 }
