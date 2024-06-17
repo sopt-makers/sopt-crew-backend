d974c3de0d05cae450e10907d3489486c1016c41
mikekks <thdalsrb79@naver.com>
2024-06-17 23:26:30 +0900

    [ADD] Dto 추가

---
+++ main/src/main/java/org/sopt/makers/crew/main/notice/dto/response/NoticeV2GetResponseDto.java
@@ -0,0 +1,14 @@
+package org.sopt.makers.crew.main.notice.dto.response;
+
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor(staticName = "of")
+public class NoticeV2GetResponseDto {
+    private final String title;
+    private final String subTitle;
+    private final String contents;
+    private final LocalDateTime createdDate;
+}
