869625db7523d728a7274c4f2571abe1d0a40a5b
Mingyu Song <100754581+mikekks@users.noreply.github.com>
2024-06-23 00:59:49 +0900

    [FEAT] 공지사항 관련 API 마이그레이션 (#223)
    * [ADD] 공지사항 작성시 시크릿갑 추가
    
    * [ADD] 라우팅 규칙 추가
    
    * [ADD] Dto 추가
    
    * [CHORE] int -> Integer 변경
    
    * [FEAT] 공지사항 관련 컨트롤러 구현
    
    * [FEAT] 공지사항 관련 서비스 로직 구현
    
    * [FEAT] 공지사항 관련 레포지토리 구현
    
    * [FIX] 예외 Enum 변경

---
+++ main/src/main/java/org/sopt/makers/crew/main/notice/service/NoticeV2Service.java
@@ -0,0 +1,54 @@
+package org.sopt.makers.crew.main.notice.service;
+
+import static org.sopt.makers.crew.main.common.response.ErrorStatus.FORBIDDEN_EXCEPTION;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.sopt.makers.crew.main.common.exception.BadRequestException;
+import org.sopt.makers.crew.main.common.exception.ForbiddenException;
+import org.sopt.makers.crew.main.entity.notice.Notice;
+import org.sopt.makers.crew.main.entity.notice.NoticeRepository;
+import org.sopt.makers.crew.main.notice.dto.request.NoticeV2CreateRequestDto;
+import org.sopt.makers.crew.main.notice.dto.response.NoticeV2GetResponseDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class NoticeV2Service {
+    private final NoticeRepository noticeRepository;
+
+    @Value("${notice.secret-key}")
+    private String noticeSecretKey;
+
+    public List<NoticeV2GetResponseDto> getNotices() {
+        // Time 클래스로 교체 필요.
+        List<Notice> notices = noticeRepository.findByExposeStartDateBeforeAndExposeEndDateAfter(LocalDateTime.now(),
+                LocalDateTime.now());
+        return notices.stream()
+                .map(notice -> NoticeV2GetResponseDto.of(notice.getTitle(), notice.getSubTitle(), notice.getContents(),
+                        notice.getCreatedDate()))
+                .toList();
+    }
+
+    @Transactional
+    public void createNotice(NoticeV2CreateRequestDto requestDto) {
+
+        if (!requestDto.getNoticeSecretKey().equals(noticeSecretKey)) {
+            throw new ForbiddenException(FORBIDDEN_EXCEPTION.getErrorCode());
+        }
+
+        Notice notice = Notice.builder()
+                .title(requestDto.getTitle())
+                .subTitle(requestDto.getSubTitle())
+                .contents(requestDto.getContents())
+                .exposeStartDate(requestDto.getExposeStartDate())
+                .exposeEndDate(requestDto.getExposeEndDate())
+                .build();
+
+        noticeRepository.save(notice);
+    }
+}
