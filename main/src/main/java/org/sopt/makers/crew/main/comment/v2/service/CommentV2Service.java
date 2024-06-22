d6afea85abddc48e902021849a55d4fda418e0f0
YeongWoooo <rdd9223@gmail.com>
2024-06-22 14:00:08 +0900

    [FIX] Comment 삭제 시 등록된 Exception으로 던지도록 변경
    - 미등록된 예외 대신, 정의된 예외 사용
    - 401 코드 리턴 대신 의미가 명확한 403 코드 리턴
    - javadoc spec에 맞게 주석 변경
    
    ref. https://github.com/sopt-makers/sopt-crew-backend/pull/218#discussion_r1646810560

--- main/src/main/java/org/sopt/makers/crew/main/comment/v2/service/CommentV2Service.java
+++ main/src/main/java/org/sopt/makers/crew/main/comment/v2/service/CommentV2Service.java
@@ -2,11 +2,12 @@ package org.sopt.makers.crew.main.comment.v2.service;
 
 import org.sopt.makers.crew.main.comment.v2.dto.request.CommentV2CreateCommentBodyDto;
 import org.sopt.makers.crew.main.comment.v2.dto.response.CommentV2CreateCommentResponseDto;
+import org.sopt.makers.crew.main.common.exception.ForbiddenException;
 
 public interface CommentV2Service {
 
   CommentV2CreateCommentResponseDto createComment(CommentV2CreateCommentBodyDto requestBody,
       Integer userId);
 
-  void deleteComment(Integer commentId, Integer userId);
+  void deleteComment(Integer commentId, Integer userId) throws ForbiddenException;
 }
