7c278a8eac6a8fe57ac0c1fa87a73bed145c6834
Yeseul Jo <68415644+yeseul106@users.noreply.github.com>
2024-06-26 00:53:02 +0900

    [FIX] Open API Spec에 맞춰서 게시글 목록 조회 API 파라미터 구조 수정 (#232)

--- main/src/main/java/org/sopt/makers/crew/main/post/v2/dto/query/PostGetPostsCommand.java
+++ main/src/main/java/org/sopt/makers/crew/main/post/v2/dto/query/PostGetPostsCommand.java
@@ -1,7 +1,6 @@
 package org.sopt.makers.crew.main.post.v2.dto.query;
 
 import java.util.Optional;
-import lombok.Builder;
 import lombok.Getter;
 import org.sopt.makers.crew.main.common.pagination.dto.PageOptionsDto;
 
@@ -10,7 +9,6 @@ public class PostGetPostsCommand extends PageOptionsDto {
 
     private Optional<Integer> meetingId;
 
-    @Builder
     public PostGetPostsCommand(Integer meetingId, Integer page, Integer take) {
         super(page, take);
         this.meetingId = Optional.ofNullable(meetingId);
