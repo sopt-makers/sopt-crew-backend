c29112db4a07ab9bb2ef3a3679a568161f6e09a1
mikekks <thdalsrb79@naver.com>
2024-07-01 20:51:39 +0900

    [FEAT] 멘션 사용자 조회 컨트롤러 로직 구현

--- main/src/main/java/org/sopt/makers/crew/main/user/v2/UserV2Controller.java
+++ main/src/main/java/org/sopt/makers/crew/main/user/v2/UserV2Controller.java
@@ -1,15 +1,11 @@
 package org.sopt.makers.crew.main.user.v2;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import java.security.Principal;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.sopt.makers.crew.main.common.util.UserUtil;
 import org.sopt.makers.crew.main.user.v2.dto.response.UserV2GetAllMeetingByUserMeetingDto;
+import org.sopt.makers.crew.main.user.v2.dto.response.UserV2GetAllMentionUserDto;
 import org.sopt.makers.crew.main.user.v2.service.UserV2Service;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,21 +17,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user/v2")
 @RequiredArgsConstructor
-@Tag(name = "사용자")
-public class UserV2Controller {
+public class UserV2Controller implements UserApi {
 
-  private final UserV2Service userV2Service;
+    private final UserV2Service userV2Service;
 
-  @Operation(summary = "내가 속한 모임 조회")
-  @GetMapping("/meeting/all")
-  @ResponseStatus(HttpStatus.OK)
-  @ApiResponses(value = {
-      @ApiResponse(responseCode = "200", description = "성공"),
-      @ApiResponse(responseCode = "204", description = "내가 속한 모임 리스트가 없는 경우", content = @Content),
-  })
-  public ResponseEntity<List<UserV2GetAllMeetingByUserMeetingDto>> getAllMeetingByUser(
-      Principal principal) {
-    Integer userId = UserUtil.getUserId(principal);
-    return ResponseEntity.ok(userV2Service.getAllMeetingByUser(userId));
-  }
+    @GetMapping("/meeting/all")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<List<UserV2GetAllMeetingByUserMeetingDto>> getAllMeetingByUser(
+            Principal principal) {
+        Integer userId = UserUtil.getUserId(principal);
+        return ResponseEntity.ok(userV2Service.getAllMeetingByUser(userId));
+    }
+
+    @GetMapping("/mention")
+    public ResponseEntity<List<UserV2GetAllMentionUserDto>> getAllMentionUser(
+            Principal principal) {
+        Integer userId = UserUtil.getUserId(principal);
+        return ResponseEntity.ok(userV2Service.getAllMentionUser(userId));
+    }
 }
