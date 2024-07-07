36cf5aae3815f263ccff33e7d6af49c5d59021b7
Ji hwan Shin <jihwanshin@Jiui-MacBookAir.local>
2024-07-07 18:00:22 +0900

    [FEAT] 요청에 제약사항 추가

--- main/src/main/java/org/sopt/makers/crew/main/comment/v2/dto/request/CommentV2MentionUserInCommentRequestDto.java
+++ main/src/main/java/org/sopt/makers/crew/main/comment/v2/dto/request/CommentV2MentionUserInCommentRequestDto.java
@@ -1,6 +1,8 @@
 package org.sopt.makers.crew.main.comment.v2.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,9 +13,11 @@ import lombok.Getter;
 public class CommentV2MentionUserInCommentRequestDto {
 
     @Schema(example = "[111, 112, 113]", required = true, description = "언급할 유저 ID")
+    @NotEmpty
     private List<Integer> userIds;
 
     @Schema(example = "1", required = true, description = "게시글 ID")
+    @NotNull
     private Integer postId;
 
     @Schema(example = "멘션내용~~", required = true, description = "멘션 내용")
--- main/src/main/java/org/sopt/makers/crew/main/post/v2/dto/request/PostV2MentionUserInPostRequestDto.java
+++ main/src/main/java/org/sopt/makers/crew/main/post/v2/dto/request/PostV2MentionUserInPostRequestDto.java
@@ -1,6 +1,8 @@
 package org.sopt.makers.crew.main.post.v2.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,9 +13,11 @@ import lombok.Getter;
 public class PostV2MentionUserInPostRequestDto {
 
     @Schema(example = "[111, 112, 113]", required = true, description = "언급할 유저 ID")
+    @NotEmpty
     private final List<Integer> userIds;
 
     @Schema(example = "1", required = true, description = "게시글 ID")
+    @NotNull
     private final Integer postId;
 
     @Schema(example = "멘션내용~~", required = true, description = "멘션 내용")
