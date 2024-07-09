5bc17b5103ffd3cf8f90c861a60301f5066ef2fb
mikekks <thdalsrb79@naver.com>
2024-07-09 14:25:30 +0900

    [TEST] StubTime 추가 및 테스트 코드 수정

--- main/src/test/java/org/sopt/makers/crew/main/comment/v2/service/CommentV2ServiceTest.java
+++ main/src/test/java/org/sopt/makers/crew/main/comment/v2/service/CommentV2ServiceTest.java
@@ -7,6 +7,7 @@ import static org.mockito.Mockito.doReturn;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.Optional;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
@@ -14,11 +15,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.sopt.makers.crew.main.comment.v2.dto.response.CommentV2ReportCommentResponseDto;
 import org.sopt.makers.crew.main.comment.v2.dto.response.CommentV2UpdateCommentResponseDto;
 import org.sopt.makers.crew.main.common.exception.BadRequestException;
 import org.sopt.makers.crew.main.common.exception.ForbiddenException;
+import org.sopt.makers.crew.main.common.util.Time;
 import org.sopt.makers.crew.main.entity.comment.Comment;
 import org.sopt.makers.crew.main.entity.comment.CommentRepository;
 import org.sopt.makers.crew.main.entity.post.Post;
@@ -31,134 +34,135 @@ import org.sopt.makers.crew.main.entity.user.UserRepository;
 @ExtendWith(MockitoExtension.class)
 public class CommentV2ServiceTest {
 
-  @InjectMocks
-  private CommentV2ServiceImpl commentV2Service;
-  @Mock
-  private CommentRepository commentRepository;
-  @Mock
-  private ReportRepository reportRepository;
-  @Mock
-  private UserRepository userRepository;
+	@InjectMocks
+	private CommentV2ServiceImpl commentV2Service;
+	@Mock
+	private CommentRepository commentRepository;
+	@Mock
+	private ReportRepository reportRepository;
+	@Mock
+	private UserRepository userRepository;
+	@Mock
+	private Time time;
 
-  private Comment comment;
+	private Comment comment;
 
-  private Post post;
+	private Post post;
 
-  private User user;
+	private User user;
 
-  private Report report;
+	private Report report;
 
-  @BeforeEach
-  void init() {
-    user = UserFixture.createStaticUser();
-    user.setUserIdForTest(1);
+	@BeforeEach
+	void init() {
+		user = UserFixture.createStaticUser();
+		user.setUserIdForTest(1);
 
-    String[] images = {"image1", "image2", "image3"};
-    this.post = Post.builder().user(user).title("title").contents("contents").images(images)
-        .build();
-    this.comment = Comment.builder().contents("contents").post(post).user(user).build();
-    post.addComment(this.comment);
+		String[] images = {"image1", "image2", "image3"};
+		this.post = Post.builder().user(user).title("title").contents("contents").images(images)
+			.build();
+		this.comment = Comment.builder().contents("contents").post(post).user(user).build();
+		post.addComment(this.comment);
 
-    this.report = Report.builder().comment(comment).user(user).build();
-  }
+		this.report = Report.builder().comment(comment).user(user).build();
+	}
 
-  @Nested
-  class 댓글_수정 {
+	@Nested
+	class 댓글_수정 {
 
-    @Test
-    void 성공() {
-      // given
-      String updatedContents = "updatedContents";
-      LocalDateTime expectedUpdatedDate = LocalDateTime.now();
+		@Test
+		void 성공() {
+			// given
+			String updatedContents = "updatedContents";
+			LocalDateTime expectedUpdatedDate = time.now();
 
-      doReturn(comment).when(commentRepository).findByIdOrThrow(any());
+			doReturn(comment).when(commentRepository).findByIdOrThrow(any());
+			doReturn(LocalDateTime.of(2024, 4, 24, 23, 59)).when(time).now();
 
-      // when
-      CommentV2UpdateCommentResponseDto result = commentV2Service.updateComment(comment.getId(),
-          updatedContents, user.getId());
+			// when
+			CommentV2UpdateCommentResponseDto result = commentV2Service.updateComment(comment.getId(),
+				updatedContents, user.getId());
 
-      // then
-      Assertions.assertThat(result.getId()).isEqualTo(comment.getId());
-      Assertions.assertThat(result.getContents()).isEqualTo(updatedContents);
-      Assertions.assertThat(LocalDateTime.parse(result.getUpdateDate()))
-          .isCloseTo(expectedUpdatedDate,
-              Assertions.within(1, ChronoUnit.SECONDS)
-          );
-    }
+			// then
+			Assertions.assertThat(result.getId()).isEqualTo(comment.getId());
+			Assertions.assertThat(result.getContents()).isEqualTo(updatedContents);
+			Assertions.assertThat(LocalDateTime.parse(result.getUpdateDate()))
+				.isEqualTo(LocalDateTime.of(2024, 4, 24, 23, 59));
+		}
 
-    @Test
-    void 실패_본인_작성_댓글_아님() {
-      // given
-      doReturn(comment).when(commentRepository).findByIdOrThrow(any());
+		@Test
+		void 실패_본인_작성_댓글_아님() {
+			// given
+			doReturn(comment).when(commentRepository).findByIdOrThrow(any());
 
-      // when & then
-      assertThrows(ForbiddenException.class, () -> {
-        commentV2Service.updateComment(comment.getId(), "updatedContents",
-            comment.getUser().getId() + 1);
-      });
-    }
-  }
+			// when & then
+			assertThrows(ForbiddenException.class, () -> {
+				commentV2Service.updateComment(comment.getId(), "updatedContents",
+					comment.getUser().getId() + 1);
+			});
+		}
+	}
 
-  @Nested
-  class 댓글_삭제 {
+	@Nested
+	class 댓글_삭제 {
 
-    @Test
-    void 성공() {
-      // given
-      int initialCommentCount = post.getCommentCount();
-      doReturn(comment).when(commentRepository).findByIdOrThrow(any());
+		@Test
+		void 성공() {
+			// given
+			int initialCommentCount = post.getCommentCount();
+			doReturn(comment).when(commentRepository).findByIdOrThrow(any());
 
-      // when
-      commentV2Service.deleteComment(comment.getId(), user.getId());
+			// when
+			commentV2Service.deleteComment(comment.getId(), user.getId());
 
-      // then
-      Assertions.assertThat(commentRepository.findById(comment.getId()))
-          .isEqualTo(Optional.empty());
-      Assertions.assertThat(post.getCommentCount()).isEqualTo(initialCommentCount - 1);
-    }
+			// then
+			Assertions.assertThat(commentRepository.findById(comment.getId()))
+				.isEqualTo(Optional.empty());
+			Assertions.assertThat(post.getCommentCount()).isEqualTo(initialCommentCount - 1);
+		}
 
-    @Test
-    void 실패_본인_작성_댓글_아님() {
-      // given
-      doReturn(comment).when(commentRepository).findByIdOrThrow(any());
+		@Test
+		void 실패_본인_작성_댓글_아님() {
+			// given
+			doReturn(comment).when(commentRepository).findByIdOrThrow(any());
 
-      // when & then
-      assertThrows(ForbiddenException.class, () -> {
-        commentV2Service.deleteComment(comment.getId(), comment.getUser().getId() + 1);
-      });
-    }
-  }
+			// when & then
+			assertThrows(ForbiddenException.class, () -> {
+				commentV2Service.deleteComment(comment.getId(), comment.getUser().getId() + 1);
+			});
+		}
+	}
 
-  @Nested
-  class 댓글_신고 {
+	@Nested
+	class 댓글_신고 {
 
-    @Test
-    void 댓글_신고_성공() {
-      // given
-      doReturn(comment).when(commentRepository).findByIdOrThrow(any());
-      doReturn(user).when(userRepository).findByIdOrThrow(any());
-      doReturn(Optional.empty()).when(reportRepository).findByCommentAndUser(any(), any());
-      doReturn(report).when(reportRepository).save(any());
+		@Test
+		void 댓글_신고_성공() {
+			// given
+			doReturn(comment).when(commentRepository).findByIdOrThrow(any());
+			doReturn(user).when(userRepository).findByIdOrThrow(any());
+			doReturn(Optional.empty()).when(reportRepository).findByCommentAndUser(any(), any());
+			doReturn(report).when(reportRepository).save(any());
 
-      // when
-      CommentV2ReportCommentResponseDto result = commentV2Service.reportComment(comment.getId(),
-          user.getId());
+			// when
+			CommentV2ReportCommentResponseDto result = commentV2Service.reportComment(comment.getId(),
+				user.getId());
 
-      // then
-      Assertions.assertThat(result.getReportId()).isEqualTo(report.getId());
-    }
+			// then
+			Assertions.assertThat(result.getReportId()).isEqualTo(report.getId());
+		}
 
-    @Test
-    void 댓글_신고_실패_이미_신고한_댓글() {
-      // given
-      doReturn(comment).when(commentRepository).findByIdOrThrow(any());
-      doReturn(user).when(userRepository).findByIdOrThrow(any());
-      doReturn(Optional.of(report)).when(reportRepository).findByCommentAndUser(any(), any());
+		@Test
+		void 댓글_신고_실패_이미_신고한_댓글() {
+			// given
+			doReturn(comment).when(commentRepository).findByIdOrThrow(any());
+			doReturn(user).when(userRepository).findByIdOrThrow(any());
+			doReturn(Optional.of(report)).when(reportRepository).findByCommentAndUser(any(), any());
 
-      // when & then
-      assertThrows(BadRequestException.class, () -> {
-        commentV2Service.reportComment(comment.getId(), user.getId());
-      });
-    }
-  }
+			// when & then
+			assertThrows(BadRequestException.class, () -> {
+				commentV2Service.reportComment(comment.getId(), user.getId());
+			});
+		}
+	}
 }
