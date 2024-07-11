b392a15a4e765b16a0ccedf52f1815d6d185457a
mikekks <thdalsrb79@naver.com>
2024-07-12 02:07:20 +0900

    [CHORE] 연관관계 제거로 인한 로직 수정

--- main/src/main/java/org/sopt/makers/crew/main/entity/user/User.java
+++ main/src/main/java/org/sopt/makers/crew/main/entity/user/User.java
@@ -3,30 +3,20 @@ package org.sopt.makers.crew.main.entity.user;
 import static org.sopt.makers.crew.main.common.response.ErrorStatus.*;
 
 import io.hypersistence.utils.hibernate.type.json.JsonBinaryType;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Type;
 import org.sopt.makers.crew.main.common.exception.ServerException;
-import org.sopt.makers.crew.main.common.response.ErrorStatus;
-import org.sopt.makers.crew.main.entity.apply.Apply;
-import org.sopt.makers.crew.main.entity.meeting.Meeting;
-import org.sopt.makers.crew.main.entity.post.Post;
-import org.sopt.makers.crew.main.entity.report.Report;
 import org.sopt.makers.crew.main.entity.user.vo.UserActivityVO;
 
 @Entity
@@ -74,50 +64,14 @@ public class User {
     @Column(name = "phone")
     private String phone;
 
-    /**
-     * 내가 생성한 모임
-     */
-    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
-    private Set<Meeting> meetings = new HashSet<>();
-
-    /**
-     * 내가 지원한 내역
-     */
-    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
-    private List<Apply> applies = new ArrayList<>();
-
-    /**
-     * 작성한 게시글
-     */
-    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
-    private List<Post> posts = new ArrayList<>();
-
-    /**
-     * 신고 내역
-     */
-    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
-    private List<Report> reports = new ArrayList<>();
-
     @Builder
-    public User(String name, int orgId, List<UserActivityVO> activities, String profileImage,
+    public User(String name, Integer orgId, List<UserActivityVO> activities, String profileImage,
                 String phone) {
         this.name = name;
         this.orgId = orgId;
         this.activities = activities;
         this.profileImage = profileImage;
         this.phone = phone;
-    }
-
-    public void addMeeting(Meeting meeting) {
-        this.meetings.add(meeting);
-    }
-
-    public void addApply(Apply apply) {
-        this.applies.add(apply);
-    }
-
-    public void addReport(Report report) {
-        this.reports.add(report);
     }
 
     public void setUserIdForTest(Integer userId) {
