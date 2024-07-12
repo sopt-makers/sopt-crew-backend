aa1b9fc234ccf1fbe490836fd9345117be9aee2c
mikekks <thdalsrb79@naver.com>
2024-07-13 01:30:38 +0900

    [FEAT] 대댓글 조회 기능 구현

--- main/src/main/java/org/sopt/makers/crew/main/comment/v2/service/CommentV2ServiceImpl.java
+++ main/src/main/java/org/sopt/makers/crew/main/comment/v2/service/CommentV2ServiceImpl.java
@@ -2,21 +2,33 @@ package org.sopt.makers.crew.main.comment.v2.service;
 
 import static org.sopt.makers.crew.main.internal.notification.PushNotificationEnums.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import lombok.RequiredArgsConstructor;
 
 import org.sopt.makers.crew.main.comment.v2.dto.request.CommentV2CreateCommentBodyDto;
 import org.sopt.makers.crew.main.comment.v2.dto.request.CommentV2MentionUserInCommentRequestDto;
+import org.sopt.makers.crew.main.comment.v2.dto.response.CommentDto;
 import org.sopt.makers.crew.main.comment.v2.dto.response.CommentV2CreateCommentResponseDto;
+import org.sopt.makers.crew.main.comment.v2.dto.response.CommentV2GetCommentsResponseDto;
 import org.sopt.makers.crew.main.comment.v2.dto.response.CommentV2UpdateCommentResponseDto;
+import org.sopt.makers.crew.main.comment.v2.dto.response.CommentWriterDto;
 import org.sopt.makers.crew.main.common.exception.BadRequestException;
 import org.sopt.makers.crew.main.common.exception.ForbiddenException;
+import org.sopt.makers.crew.main.common.pagination.dto.PageMetaDto;
+import org.sopt.makers.crew.main.common.pagination.dto.PageOptionsDto;
 import org.sopt.makers.crew.main.common.response.ErrorStatus;
 import org.sopt.makers.crew.main.comment.v2.dto.response.CommentV2ReportCommentResponseDto;
 import org.sopt.makers.crew.main.common.util.Time;
 import org.sopt.makers.crew.main.entity.comment.Comment;
 import org.sopt.makers.crew.main.entity.comment.CommentRepository;
+import org.sopt.makers.crew.main.entity.like.Like;
+import org.sopt.makers.crew.main.entity.like.LikeRepository;
+import org.sopt.makers.crew.main.entity.like.MyLikes;
 import org.sopt.makers.crew.main.entity.post.Post;
 import org.sopt.makers.crew.main.entity.post.PostRepository;
 import org.sopt.makers.crew.main.entity.report.Report;
@@ -26,6 +38,8 @@ import org.sopt.makers.crew.main.entity.user.UserRepository;
 import org.sopt.makers.crew.main.internal.notification.PushNotificationService;
 import org.sopt.makers.crew.main.internal.notification.dto.PushNotificationRequestDto;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -33,6 +47,7 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class CommentV2ServiceImpl implements CommentV2Service {
+	private static final int IS_PARENT_COMMENT = 0;
 	private static final int IS_REPLY_COMMENT = 1;
 	private static final String DELETE_COMMENT_CONTENT = "삭제된 댓글입니다.";
 
@@ -40,6 +55,8 @@ public class CommentV2ServiceImpl implements CommentV2Service {
 	private final UserRepository userRepository;
 	private final CommentRepository commentRepository;
 	private final ReportRepository reportRepository;
+	private final LikeRepository likeRepository;
+
 	private final PushNotificationService pushNotificationService;
 
 	@Value("${push-notification.web-url}")
@@ -68,8 +85,8 @@ public class CommentV2ServiceImpl implements CommentV2Service {
 		if (isReplyComment) {
 			validateParentCommentId(requestBody);
 			depth = 1;
-			order = getOrder(parentId);
 			parentId = requestBody.getParentCommentId();
+			order = getOrder(parentId);
 		}
 
 		Comment comment = getComment(requestBody, post, writer, depth, order, parentId);
@@ -136,7 +153,7 @@ public class CommentV2ServiceImpl implements CommentV2Service {
 		Comment comment = commentRepository.findByIdOrThrow(commentId);
 
 		// 2. comment의 user_id와 userId가 같은지 확인한다.
-		comment.isWriter(userId);
+		comment.validateWriter(userId);
 
 		// 3. comment의 contents를 수정한다.
 		comment.updateContents(contents, time.now());
@@ -144,6 +161,45 @@ public class CommentV2ServiceImpl implements CommentV2Service {
 		// 4. 수정된 comment의 id, contents, updatedDate를 반환한다.
 		return CommentV2UpdateCommentResponseDto.of(comment.getId(), comment.getContents(),
 			String.valueOf(comment.getUpdatedDate()));
+	}
+
+	@Override
+	public CommentV2GetCommentsResponseDto getComments(Integer postId, Integer page, Integer take, Integer userId) {
+		Page<Comment> allComments = commentRepository.findAllByPostIdPagination(postId, IS_PARENT_COMMENT,
+			PageRequest.of(page - 1, take));
+		List<Comment> parentComments = allComments.stream()
+			.filter(comment -> comment.getDepth() == IS_PARENT_COMMENT)
+			.toList();
+		List<Comment> childComments = allComments.stream()
+			.filter(comment -> comment.getDepth() == IS_REPLY_COMMENT)
+			.toList();
+
+		MyLikes myLikes = new MyLikes(likeRepository.findAllByUserIdAndPostIdNotNull(userId));
+
+		List<CommentDto> commentDtos = getCommentDtos(userId, parentComments, childComments, myLikes);
+
+		PageOptionsDto pageOptionsDto = new PageOptionsDto(page, take);
+		PageMetaDto pageMetaDto = new PageMetaDto(pageOptionsDto, (int)allComments.getTotalElements());
+
+		return CommentV2GetCommentsResponseDto.of(commentDtos, pageMetaDto);
+	}
+
+	private List<CommentDto> getCommentDtos(Integer userId, List<Comment> parentComments, List<Comment> childComments,
+		MyLikes myLikes) {
+
+		Map<Integer, List<CommentDto>> replyMap = new HashMap<>();
+		childComments.forEach(comment -> replyMap.computeIfAbsent(comment.getParentId(), k -> new ArrayList<>())
+			.add(new CommentDto(comment.getId(), comment.getContents(),
+				new CommentWriterDto(comment.getUser().getId(), comment.getUser().getName(),
+					comment.getUser().getProfileImage()), comment.getUpdatedDate(), comment.getLikeCount(),
+				myLikes.isLikeComment(comment.getId()), comment.isWriter(userId), null)));
+
+		return parentComments.stream()
+			.map(comment -> new CommentDto(comment.getId(), comment.getContents(),
+				new CommentWriterDto(comment.getUser().getId(), comment.getUser().getName(),
+					comment.getUser().getProfileImage()), comment.getUpdatedDate(), comment.getLikeCount(),
+				myLikes.isLikeComment(comment.getId()), comment.isWriter(userId), replyMap.get(comment.getId())))
+			.toList();
 	}
 
 	/**
