fc06d7e9c4844ae847de6af66e67e80f363e9197
Ji hwan Shin <jihwanshin@Jiui-MacBookAir.local>
2024-07-20 00:46:07 +0900

    [FEAT] 멘션 시 비밀 문자열 제거 Util 구현

---
+++ main/src/main/java/org/sopt/makers/crew/main/common/util/MentionSecretStringRemover.java
@@ -0,0 +1,25 @@
+package org.sopt.makers.crew.main.common.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class MentionSecretStringRemover {
+
+    private static final String PREFIX_PATTERN = "-!@#";
+    private static final String SUFFIX_PATTERN = "\\[\\d+\\]%\\^&\\+";
+
+    public static String removeSecretString(String content) {
+        Pattern prefixPattern = Pattern.compile(PREFIX_PATTERN);
+        Pattern suffixPattern = Pattern.compile(SUFFIX_PATTERN);
+
+        Matcher prefixMatcher = prefixPattern.matcher(content);
+        content = prefixMatcher.replaceAll("");
+
+        Matcher suffixMatcher = suffixPattern.matcher(content);
+        content = suffixMatcher.replaceAll("");
+
+        return content;
+    }
+}
