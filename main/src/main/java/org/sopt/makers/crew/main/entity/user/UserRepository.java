a8602d0b911d5e03b32eac3a937d0b33071bfbb6
Ji hwan Shin <jihwanshin@Jiui-MacBookAir.local>
2024-08-04 16:04:49 +0900

    [FIX] resolve conflict

--- main/src/main/java/org/sopt/makers/crew/main/entity/user/UserRepository.java
+++ main/src/main/java/org/sopt/makers/crew/main/entity/user/UserRepository.java
@@ -2,30 +2,25 @@ package org.sopt.makers.crew.main.entity.user;
 
 import static org.sopt.makers.crew.main.common.response.ErrorStatus.NO_CONTENT_EXCEPTION;
 
-<<<<<<< HEAD
-import io.swagger.v3.oas.annotations.Operation;
-=======
 import java.util.List;
->>>>>>> develop
 import java.util.Optional;
-
 import org.sopt.makers.crew.main.common.exception.NoContentException;
 import org.sopt.makers.crew.main.common.exception.UnAuthorizedException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-	Optional<User> findByOrgId(Integer orgId);
+  Optional<User> findByOrgId(Integer orgId);
 
-	default User findByIdOrThrow(Integer userId) {
-		return findById(userId).orElseThrow(() -> new UnAuthorizedException());
-	}
+  default User findByIdOrThrow(Integer userId) {
+    return findById(userId).orElseThrow(() -> new UnAuthorizedException());
+  }
 
-	default User findByOrgIdOrThrow(Integer orgUserId) {
-		return findByOrgId(orgUserId).orElseThrow(
-			() -> new NoContentException(
-				NO_CONTENT_EXCEPTION.getErrorCode())); //유저가 아직 모임 서비스를 이용 전이기 때문에
-	}
+  default User findByOrgIdOrThrow(Integer orgUserId) {
+    return findByOrgId(orgUserId).orElseThrow(
+        () -> new NoContentException(
+            NO_CONTENT_EXCEPTION.getErrorCode())); //유저가 아직 모임 서비스를 이용 전이기 때문에
+  }
 
-	List<User> findByIdIn(List<Integer> userIds);
+  List<User> findByIdIn(List<Integer> userIds);
 }
--- main/src/main/java/org/sopt/makers/crew/main/user/v2/service/UserV2Service.java
+++ main/src/main/java/org/sopt/makers/crew/main/user/v2/service/UserV2Service.java
@@ -3,20 +3,21 @@ package org.sopt.makers.crew.main.user.v2.service;
 import java.util.List;
 import org.sopt.makers.crew.main.entity.user.User;
 import org.sopt.makers.crew.main.user.v2.dto.response.UserV2GetAllMeetingByUserMeetingDto;
-<<<<<<< HEAD
+import org.sopt.makers.crew.main.user.v2.dto.response.UserV2GetAllMentionUserDto;
 import org.sopt.makers.crew.main.user.v2.dto.response.UserV2GetApplyByUserDto;
 import org.sopt.makers.crew.main.user.v2.dto.response.UserV2GetMeetingByUserDto;
 import org.sopt.makers.crew.main.user.v2.dto.response.UserV2GetUserOwnProfileResponseDto;
-=======
-import org.sopt.makers.crew.main.user.v2.dto.response.UserV2GetAllMentionUserDto;
->>>>>>> develop
 
 public interface UserV2Service {
 
   List<UserV2GetAllMeetingByUserMeetingDto> getAllMeetingByUser(Integer userId);
+
   User getUserById(Integer userId);
+
   UserV2GetUserOwnProfileResponseDto getUserOwnProfile(Integer userId);
+
   UserV2GetMeetingByUserDto getMeetingByUser(Integer userId);
+
   UserV2GetApplyByUserDto getApplyByUser(Integer userId);
 
   List<UserV2GetAllMentionUserDto> getAllMentionUser();
