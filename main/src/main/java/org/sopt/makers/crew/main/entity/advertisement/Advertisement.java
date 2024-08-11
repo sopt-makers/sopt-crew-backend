dd5c015781d5291c9db28736a5a23e70a466fba5
mikekks <thdalsrb79@naver.com>
2024-08-11 22:05:51 +0900

    feat: 광고 엔티티 추가

---
+++ main/src/main/java/org/sopt/makers/crew/main/entity/advertisement/Advertisement.java
@@ -0,0 +1,56 @@
+package org.sopt.makers.crew.main.entity.advertisement;
+
+import static jakarta.persistence.GenerationType.*;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "advertisement")
+public class Advertisement {
+	/**
+	 * Primary Key
+	 */
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	private Integer id;
+
+	@NotNull
+	private String advertisementImageUrl;
+
+	@NotNull
+	private String advertisementLink;
+
+	@NotNull
+	private Long priority;
+
+	@NotNull
+	private LocalDateTime advertisementStartDate;
+
+	@NotNull
+	private LocalDateTime advertisementEndDate;
+
+	@Builder
+	private Advertisement(String advertisementImageUrl, String advertisementLink, LocalDateTime advertisementStartDate,
+		LocalDateTime advertisementEndDate) {
+		this.advertisementImageUrl = advertisementImageUrl;
+		this.advertisementLink = advertisementLink;
+		this.advertisementStartDate = advertisementStartDate;
+		this.advertisementEndDate = advertisementEndDate;
+	}
+}
