b28f9f0028c29b4516f141390f7807a08839bbc7
mikekks <thdalsrb79@naver.com>
2024-08-19 18:32:50 +0900

    [DOCS] 내가 만든 모임 조회 및 내가 신청한 모임 조회 스웨거 추가

--- main/src/main/java/org/sopt/makers/crew/main/user/v2/UserV2Api.java
+++ main/src/main/java/org/sopt/makers/crew/main/user/v2/UserV2Api.java
@@ -7,9 +7,15 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.security.Principal;
 import java.util.List;
+
 import org.sopt.makers.crew.main.user.v2.dto.response.UserV2GetAllMeetingByUserMeetingDto;
 import org.sopt.makers.crew.main.user.v2.dto.response.UserV2GetAllMentionUserDto;
+import org.sopt.makers.crew.main.user.v2.dto.response.UserV2GetAppliedMeetingByUserResponseDto;
+import org.sopt.makers.crew.main.user.v2.dto.response.UserV2GetCreatedMeetingByUserResponseDto;
+import org.sopt.makers.crew.main.user.v2.dto.response.UserV2GetUserOwnProfileResponseDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Tag(name = "사용자")
 public interface UserV2Api {
@@ -25,4 +31,32 @@ public interface UserV2Api {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "성공")})
     ResponseEntity<List<UserV2GetAllMentionUserDto>> getAllMentionUser(Principal principal);
+
+    @Operation(summary = "유저 본인 프로필 조회")
+    @ResponseStatus(HttpStatus.OK)
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "성공"),
+        @ApiResponse(responseCode = "400", description = "해당 유저가 없는 경우", content = @Content),
+    })
+    ResponseEntity<UserV2GetUserOwnProfileResponseDto> getUserOwnProfile(Principal principal);
+
+    @Operation(summary = "내가 신청한 모임 조회")
+    @ResponseStatus(HttpStatus.OK)
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "성공")
+    })
+    ResponseEntity<UserV2GetAppliedMeetingByUserResponseDto> getAppliedMeetingByUser(
+        Principal principal);
+
+    @Operation(summary = "내가 만든 모임 조회")
+    @ResponseStatus(HttpStatus.OK)
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "성공")
+    })
+    ResponseEntity<UserV2GetCreatedMeetingByUserResponseDto> getCreatedMeetingByUser(
+        Principal principal);
+
+
+
+
 }
