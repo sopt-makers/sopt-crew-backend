f2a0a11382aa5fa6c0d3c99827fc0443779bc095
mikekks <thdalsrb79@naver.com>
2024-08-21 22:34:21 +0900

    add: Dto 정의

--- main/src/main/java/org/sopt/makers/crew/main/common/dto/MeetingCreatorDto.java
+++ main/src/main/java/org/sopt/makers/crew/main/common/dto/MeetingCreatorDto.java
@@ -2,23 +2,37 @@ package org.sopt.makers.crew.main.common.dto;
 
 import org.sopt.makers.crew.main.entity.user.User;
 
+import com.querydsl.core.annotations.QueryProjection;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
 
-public record MeetingCreatorDto(
+@Schema(name = "MeetingCreatorDto", description = "모임 개설자 Dto")
+@Getter
+public class MeetingCreatorDto {
 	@Schema(description = "모임장 id, 크루에서 사용하는 userId", example = "1")
 	@NotNull
-	Integer id,
+	Integer id;
 	@Schema(description = "모임장 이름", example = "홍길동")
 	@NotNull
-	String name,
+	String name;
 	@Schema(description = "모임장 org id, 메이커스 프로덕트에서 범용적으로 사용하는 userId", example = "1")
 	@NotNull
-	Integer orgId,
+	Integer orgId;
 	@Schema(description = "모임장 프로필 사진", example = "[url] 형식")
-	String profileImage
-) {
-	public static MeetingCreatorDto of(User user){
+	String profileImage;
+
+	@QueryProjection
+	public MeetingCreatorDto(Integer id, String name, Integer orgId, String profileImag) {
+		this.id = id;
+		this.name = name;
+		this.orgId = orgId;
+		this.profileImage = profileImag;
+	}
+
+	public static MeetingCreatorDto of(User user) {
 		return new MeetingCreatorDto(user.getId(), user.getName(), user.getOrgId(), user.getProfileImage());
 	}
+
 }
