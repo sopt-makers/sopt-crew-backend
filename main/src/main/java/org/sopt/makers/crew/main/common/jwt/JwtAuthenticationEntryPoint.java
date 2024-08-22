9d70e9b99879799f35bd5e394383f655c00d84ae
mikekks <thdalsrb79@naver.com>
2024-08-22 10:33:25 +0900

    chore: jwt 로직 포맷팅 및 클레임 put 로직 수정

--- main/src/main/java/org/sopt/makers/crew/main/common/jwt/JwtAuthenticationEntryPoint.java
+++ main/src/main/java/org/sopt/makers/crew/main/common/jwt/JwtAuthenticationEntryPoint.java
@@ -2,7 +2,9 @@ package org.sopt.makers.crew.main.common.jwt;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
+
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -10,16 +12,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
-  @Override
-  public void commence(HttpServletRequest request, HttpServletResponse response,
-      AuthenticationException authException) throws IOException {
-    setResponse(response);
-  }
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response,
+		AuthenticationException authException) throws IOException {
+		setResponse(response);
+	}
 
-
-  public void setResponse(HttpServletResponse response) throws IOException {
-    response.setContentType("application/json;charset=UTF-8");
-    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-  }
+	public void setResponse(HttpServletResponse response) throws IOException {
+		response.setContentType("application/json;charset=UTF-8");
+		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+	}
 
 }
--- main/src/main/java/org/sopt/makers/crew/main/common/jwt/JwtTokenProvider.java
+++ main/src/main/java/org/sopt/makers/crew/main/common/jwt/JwtTokenProvider.java
@@ -8,14 +8,17 @@ import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.UnsupportedJwtException;
 import jakarta.servlet.http.HttpServletRequest;
+
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
+
 import javax.crypto.spec.SecretKeySpec;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -23,73 +26,74 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class JwtTokenProvider {
 
-  @Value("${jwt.secret}")
-  private String secretKey;
+	@Value("${jwt.secret}")
+	private String secretKey;
 
-  @Value("${jwt.access-token.expire-length}")
-  private Long accessTokenExpireLength;
+	@Value("${jwt.access-token.expire-length}")
+	private Long accessTokenExpireLength;
 
-  private static final String AUTHORIZATION_HEADER = "Authorization";
+	private static final String AUTHORIZATION_HEADER = "Authorization";
 
-  public String generateAccessToken(Authentication authentication) {
-    Date now = new Date();
-    Date expiration = new Date(now.getTime() + accessTokenExpireLength);
+	public String generateAccessToken(Integer userId, String userName) {
+		Date now = new Date();
+		Date expiration = new Date(now.getTime() + accessTokenExpireLength);
 
-    final Claims claims = Jwts.claims()
-        .setIssuedAt(now)
-        .setExpiration(expiration);
+		final Claims claims = Jwts.claims()
+			.setIssuedAt(now)
+			.setExpiration(expiration);
 
-    claims.put("id", authentication.getPrincipal());
+		claims.put("id", userId);
+		claims.put("name", userName);
 
-    return Jwts.builder()
-        .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
-        .setClaims(claims)
-        .signWith(getSignKey(), SignatureAlgorithm.HS256)
-        .compact();
-  }
+		return Jwts.builder()
+			.setHeaderParam(Header.TYPE, Header.JWT_TYPE)
+			.setClaims(claims)
+			.signWith(getSignKey(), SignatureAlgorithm.HS256)
+			.compact();
+	}
 
-  public Integer getAccessTokenPayload(String token) {
-    return Integer.parseInt(
-        Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token)
-            .getBody().get("id").toString());
-  }
+	public Integer getAccessTokenPayload(String token) {
+		return Integer.parseInt(
+			Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token)
+				.getBody().get("id").toString());
+	}
 
-  public String resolveToken(HttpServletRequest request) {
+	public String resolveToken(HttpServletRequest request) {
 
-    String header = request.getHeader(AUTHORIZATION_HEADER);
+		String header = request.getHeader(AUTHORIZATION_HEADER);
 
-    if (header == null || !header.startsWith("Bearer ")) {
-      return null;
-    } else {
-      return header.split(" ")[1];
-    }
-  }
+		if (header == null || !header.startsWith("Bearer ")) {
+			return null;
+		} else {
+			return header.split(" ")[1];
+		}
+	}
 
-  public JwtExceptionType validateToken(String token) {
-    try {
-      Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token)
-          .getBody();
-      return JwtExceptionType.VALID_JWT_TOKEN;
-    } catch (io.jsonwebtoken.security.SignatureException exception) {
-      log.error("잘못된 JWT 서명을 가진 토큰입니다.");
-      return JwtExceptionType.INVALID_JWT_SIGNATURE;
-    } catch (MalformedJwtException exception) {
-      log.error("잘못된 JWT 토큰입니다.");
-      return JwtExceptionType.INVALID_JWT_TOKEN;
-    } catch (ExpiredJwtException exception) {
-      log.error("만료된 JWT 토큰입니다.");
-      return JwtExceptionType.EXPIRED_JWT_TOKEN;
-    } catch (UnsupportedJwtException exception) {
-      log.error("지원하지 않는 JWT 토큰입니다.");
-      return JwtExceptionType.UNSUPPORTED_JWT_TOKEN;
-    } catch (IllegalArgumentException exception) {
-      log.error("JWT Claims가 비어있습니다.");
-      return JwtExceptionType.EMPTY_JWT;
-    }
-  }
+	public JwtExceptionType validateToken(String token) {
+		try {
+			Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token)
+				.getBody();
+			return JwtExceptionType.VALID_JWT_TOKEN;
+		} catch (io.jsonwebtoken.security.SignatureException exception) {
+			log.error("잘못된 JWT 서명을 가진 토큰입니다.");
+			return JwtExceptionType.INVALID_JWT_SIGNATURE;
+		} catch (MalformedJwtException exception) {
+			log.error("잘못된 JWT 토큰입니다.");
+			return JwtExceptionType.INVALID_JWT_TOKEN;
+		} catch (ExpiredJwtException exception) {
+			log.error("만료된 JWT 토큰입니다.");
+			return JwtExceptionType.EXPIRED_JWT_TOKEN;
+		} catch (UnsupportedJwtException exception) {
+			log.error("지원하지 않는 JWT 토큰입니다.");
+			return JwtExceptionType.UNSUPPORTED_JWT_TOKEN;
+		} catch (IllegalArgumentException exception) {
+			log.error("JWT Claims가 비어있습니다.");
+			return JwtExceptionType.EMPTY_JWT;
+		}
+	}
 
-  private Key getSignKey() {
-    byte[] keyBytes = secretKey.getBytes(StandardCharsets.UTF_8);
-    return new SecretKeySpec(keyBytes, "HmacSHA256");
-  }
+	private Key getSignKey() {
+		byte[] keyBytes = secretKey.getBytes(StandardCharsets.UTF_8);
+		return new SecretKeySpec(keyBytes, "HmacSHA256");
+	}
 }
