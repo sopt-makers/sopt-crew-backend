e4f2b741ce33976a9215851639e439dfec93f662
mikekks <thdalsrb79@naver.com>
2024-08-22 19:23:00 +0900

    chore: deprecated 처리

--- server/src/meeting/v0/meeting-v0.controller.ts
+++ server/src/meeting/v0/meeting-v0.controller.ts
@@ -68,6 +68,7 @@ export class MeetingV0Controller {
     summary: '모임 지원자/참여자 조회',
     description:
       '모임 지원자/참여자 조회 (모임장이면 지원자, 아니면 참여자 조회)',
+    deprecated: true,
   })
   @ApiOkResponseCommon(MeetingV0GetApplyListByMeetingResponseDto)
   @ApiResponse({
