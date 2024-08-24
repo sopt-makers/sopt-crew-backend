e9f99fecf010f563ad76273fb1bc7f6dfebc01ab
mikekks <thdalsrb79@naver.com>
2024-08-24 15:53:35 +0900

    add: notnull 추가

--- main/src/main/java/org/sopt/makers/crew/main/meeting/v2/dto/response/PreSignedUrlFieldResponseDto.java
+++ main/src/main/java/org/sopt/makers/crew/main/meeting/v2/dto/response/PreSignedUrlFieldResponseDto.java
@@ -2,6 +2,7 @@ package org.sopt.makers.crew.main.meeting.v2.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -10,27 +11,35 @@ import lombok.RequiredArgsConstructor;
 public class PreSignedUrlFieldResponseDto {
 
 	@JsonProperty("Content-Type")
+	@NotNull
 	private final String contentType;
 
 	@JsonProperty("key")
+	@NotNull
 	private final String key;
 
 	@JsonProperty("bucket")
+	@NotNull
 	private final String bucket;
 
 	@JsonProperty("X-Amz-Algorithm")
+	@NotNull
 	private final String algorithm;
 
 	@JsonProperty("X-Amz-Credential")
+	@NotNull
 	private final String credential;
 
 	@JsonProperty("X-Amz-Date")
+	@NotNull
 	private final String date;
 
 	@JsonProperty("Policy")
+	@NotNull
 	private final String policy;
 
 	@JsonProperty("X-Amz-Signature")
+	@NotNull
 	private final String signature;
 
 	public static PreSignedUrlFieldResponseDto of(String contentType, String key, String bucket, String algorithm,
--- main/src/main/java/org/sopt/makers/crew/main/meeting/v2/dto/response/PreSignedUrlResponseDto.java
+++ main/src/main/java/org/sopt/makers/crew/main/meeting/v2/dto/response/PreSignedUrlResponseDto.java
@@ -1,12 +1,17 @@
 package org.sopt.makers.crew.main.meeting.v2.dto.response;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Getter
 public class PreSignedUrlResponseDto {
+
+	@NotNull
 	private final String url;
+
+	@NotNull
 	private final PreSignedUrlFieldResponseDto fields;
 
 	public static PreSignedUrlResponseDto of(String url, PreSignedUrlFieldResponseDto fields){
