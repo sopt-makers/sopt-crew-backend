8d468fa5bdcf4f370ec5b3d52782ba97b7620785
mikekks <thdalsrb79@naver.com>
2024-08-24 15:21:33 +0900

    add: csv 관련 dto 추가

---
+++ main/src/main/java/org/sopt/makers/crew/main/meeting/v2/dto/response/AppliesCsvFileUrlResponseDto.java
@@ -0,0 +1,10 @@
+package org.sopt.makers.crew.main.meeting.v2.dto.response;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public class AppliesCsvFileUrlResponseDto {
+	private final String url;
+}
