c438326ffd3f748049fb366f8ce244b672cd28fc
mikekks <thdalsrb79@naver.com>
2024-08-26 00:41:06 +0900

    refactor: 예외 응답 개선

--- main/src/main/java/org/sopt/makers/crew/main/common/jwt/JwtAuthenticationEntryPoint.java
+++ main/src/main/java/org/sopt/makers/crew/main/common/jwt/JwtAuthenticationEntryPoint.java
@@ -1,13 +1,18 @@
 package org.sopt.makers.crew.main.common.jwt;
 
+import static org.sopt.makers.crew.main.common.exception.ErrorStatus.*;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
+import org.sopt.makers.crew.main.common.exception.ExceptionResponse;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
@@ -21,6 +26,11 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 	public void setResponse(HttpServletResponse response) throws IOException {
 		response.setContentType("application/json;charset=UTF-8");
 		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+		ObjectMapper mapper = new ObjectMapper();
+		String jsonResponse = mapper.writeValueAsString(ExceptionResponse.fail(INVALID_INPUT_VALUE_FILTER.getErrorCode()));
+
+		response.getWriter().write(jsonResponse);
 	}
 
 }
