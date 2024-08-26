0fe44e479a91a3560a659e61adb75608dd41c79b
yeseul106 <20191037@sungshin.ac.kr>
2024-08-26 16:15:11 +0900

    [ADD] 모임 게시글 좋아요 토글 api 관련 dto 추가

--- main/src/main/java/org/sopt/makers/crew/main/post/v2/dto/response/PostV2SwitchPostLikeResponseDto.java
+++ main/src/main/java/org/sopt/makers/crew/main/post/v2/dto/response/PostV2SwitchPostLikeResponseDto.java
@@ -1,2 +1,16 @@
-package org.sopt.makers.crew.main.post.v2.dto.response;public class PostV2SwitchPostLikeResponseDto {
+package org.sopt.makers.crew.main.post.v2.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(staticName = "of")
+@Schema(name = "PostV2SwitchPostLikeResponseDto", description = "모임 게시글 좋아요 토글 응답 Dto")
+public class PostV2SwitchPostLikeResponseDto {
+
+    @Schema(description = "본인이 게시글 좋아요를 눌렀는지 여부", example = "true")
+    @NotNull
+    private final Boolean isLiked;
 }
