967e76fefcd8d6e557c7d00f2ee28a190149c8a7
mikekks <thdalsrb79@naver.com>
2024-08-26 17:43:54 +0900

    chore: 포맷터 적용

--- main/src/main/java/org/sopt/makers/crew/main/entity/like/LikeRepository.java
+++ main/src/main/java/org/sopt/makers/crew/main/entity/like/LikeRepository.java
@@ -1,6 +1,7 @@
 package org.sopt.makers.crew.main.entity.like;
 
 import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -8,31 +9,31 @@ import org.springframework.transaction.annotation.Transactional;
 
 public interface LikeRepository extends JpaRepository<Like, Integer> {
 
-    List<Like> findAllByUserIdAndCommentIdNotNull(Integer userId);
+	List<Like> findAllByUserIdAndCommentIdNotNull(Integer userId);
 
-    @Modifying(clearAutomatically = true)
-    @Transactional
-    @Query("DELETE FROM Like l WHERE l.postId = :postId")
-    void deleteAllByPostId(Integer postId);
+	@Modifying(clearAutomatically = true)
+	@Transactional
+	@Query("DELETE FROM Like l WHERE l.postId = :postId")
+	void deleteAllByPostId(Integer postId);
 
-    @Modifying(clearAutomatically = true)
-    @Transactional
-    @Query("DELETE FROM Like l WHERE l.postId IN :postIds")
-    void deleteAllByPostIdsInQuery(List<Integer> postIds);
+	@Modifying(clearAutomatically = true)
+	@Transactional
+	@Query("DELETE FROM Like l WHERE l.postId IN :postIds")
+	void deleteAllByPostIdsInQuery(List<Integer> postIds);
 
-    @Modifying(clearAutomatically = true)
-    @Transactional
-    @Query("DELETE FROM Like l WHERE l.commentId IN :commentIds")
-    void deleteAllByCommentIdsInQuery(List<Integer> commentIds);
+	@Modifying(clearAutomatically = true)
+	@Transactional
+	@Query("DELETE FROM Like l WHERE l.commentId IN :commentIds")
+	void deleteAllByCommentIdsInQuery(List<Integer> commentIds);
 
-    boolean existsByUserIdAndCommentId(Integer userId, Integer commentId);
+	boolean existsByUserIdAndCommentId(Integer userId, Integer commentId);
 
-    void deleteByUserIdAndCommentId(Integer userId, Integer commentId);
+	void deleteByUserIdAndCommentId(Integer userId, Integer commentId);
 
-    int deleteByUserIdAndPostId(Integer userId, Integer postId);
+	int deleteByUserIdAndPostId(Integer userId, Integer postId);
 
-    List<Like> findAllByPostIdIsIn(List<Integer> postIds);
+	List<Like> findAllByPostIdIsIn(List<Integer> postIds);
 
-    List<Like> findAllByCommentIdIsIn(List<Integer> commentIds);
+	List<Like> findAllByCommentIdIsIn(List<Integer> commentIds);
 
 }
--- main/src/main/java/org/sopt/makers/crew/main/entity/report/ReportRepository.java
+++ main/src/main/java/org/sopt/makers/crew/main/entity/report/ReportRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReportRepository extends JpaRepository<Report, Integer> {
 
-    boolean existsByCommentIdAndUserId(Integer commentId, Integer userId);
+	boolean existsByCommentIdAndUserId(Integer commentId, Integer userId);
 
-    boolean existsByPostIdAndUserId(Integer postId, Integer userId);
+	boolean existsByPostIdAndUserId(Integer postId, Integer userId);
 }
