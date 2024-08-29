03433b7d95b1691d63902743f6fc2688616d1b32
mikekks <thdalsrb79@naver.com>
2024-08-29 19:53:23 +0900

    refactor: 배너 조회 로직 쿼리 개선

--- main/src/main/java/org/sopt/makers/crew/main/entity/meeting/MeetingRepository.java
+++ main/src/main/java/org/sopt/makers/crew/main/entity/meeting/MeetingRepository.java
@@ -3,11 +3,11 @@ package org.sopt.makers.crew.main.entity.meeting;
 import static org.sopt.makers.crew.main.common.exception.ErrorStatus.NOT_FOUND_MEETING;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.sopt.makers.crew.main.common.exception.BadRequestException;
 import org.sopt.makers.crew.main.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MeetingRepository extends JpaRepository<Meeting, Integer>, MeetingSearchRepository {
 
@@ -15,10 +15,11 @@ public interface MeetingRepository extends JpaRepository<Meeting, Integer>, Meet
 
 	List<Meeting> findAllByUser(User user);
 
-	Optional<Meeting> findById(Integer meetingId);
-
 	default Meeting findByIdOrThrow(Integer meetingId) {
 		return findById(meetingId)
 			.orElseThrow(() -> new BadRequestException(NOT_FOUND_MEETING.getErrorCode()));
 	}
+
+	@Query("SELECT m FROM Meeting m JOIN FETCH m.user ORDER BY m.id DESC LIMIT 20")
+	List<Meeting> findTop20ByOrderByIdDesc();
 }
