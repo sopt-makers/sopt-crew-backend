dfd04c2e92522510406f02c383d245009842157a
mikekks <thdalsrb79@naver.com>
2024-09-05 00:17:23 +0900

    chore(meeting): 유저 관련 API temp 버전 추가

--- main/src/main/java/org/sopt/makers/crew/main/user/v2/UserV2Api.java
+++ main/src/main/java/org/sopt/makers/crew/main/user/v2/UserV2Api.java
@@ -5,9 +5,11 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+
 import java.security.Principal;
 import java.util.List;
 
+import org.sopt.makers.crew.main.common.dto.TempResponseDto;
 import org.sopt.makers.crew.main.user.v2.dto.response.UserV2GetAllMeetingByUserMeetingDto;
 import org.sopt.makers.crew.main.user.v2.dto.response.UserV2GetAllMentionUserDto;
 import org.sopt.makers.crew.main.user.v2.dto.response.UserV2GetAppliedMeetingByUserResponseDto;
@@ -20,43 +22,64 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @Tag(name = "사용자")
 public interface UserV2Api {
 
-    @Operation(summary = "내가 속한 모임 조회")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "성공"),
-            @ApiResponse(responseCode = "204", description = "내가 속한 모임 리스트가 없는 경우", content = @Content),
-    })
-    ResponseEntity<List<UserV2GetAllMeetingByUserMeetingDto>> getAllMeetingByUser(Principal principal);
+	@Operation(summary = "내가 속한 모임 조회")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "성공"),
+		@ApiResponse(responseCode = "204", description = "내가 속한 모임 리스트가 없는 경우", content = @Content),
+	})
+	ResponseEntity<List<UserV2GetAllMeetingByUserMeetingDto>> getAllMeetingByUser(Principal principal);
 
-    @Operation(summary = "멘션 사용자 조회")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "성공")})
-    ResponseEntity<List<UserV2GetAllMentionUserDto>> getAllMentionUser(Principal principal);
+	@Operation(summary = "멘션 사용자 조회")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "성공")})
+	ResponseEntity<List<UserV2GetAllMentionUserDto>> getAllMentionUser(Principal principal);
 
-    @Operation(summary = "유저 본인 프로필 조회")
-    @ResponseStatus(HttpStatus.OK)
-    @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "성공"),
-        @ApiResponse(responseCode = "400", description = "해당 유저가 없는 경우", content = @Content),
-    })
-    ResponseEntity<UserV2GetUserOwnProfileResponseDto> getUserOwnProfile(Principal principal);
+	@Operation(summary = "유저 본인 프로필 조회")
+	@ResponseStatus(HttpStatus.OK)
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "성공"),
+		@ApiResponse(responseCode = "400", description = "해당 유저가 없는 경우", content = @Content),
+	})
+	ResponseEntity<UserV2GetUserOwnProfileResponseDto> getUserOwnProfile(Principal principal);
 
-    @Operation(summary = "내가 신청한 모임 조회")
-    @ResponseStatus(HttpStatus.OK)
-    @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "성공")
-    })
-    ResponseEntity<UserV2GetAppliedMeetingByUserResponseDto> getAppliedMeetingByUser(
-        Principal principal);
+	@Operation(summary = "[TEMP] 유저 본인 프로필 조회")
+	@ResponseStatus(HttpStatus.OK)
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "성공"),
+		@ApiResponse(responseCode = "400", description = "해당 유저가 없는 경우", content = @Content),
+	})
+	ResponseEntity<TempResponseDto<UserV2GetUserOwnProfileResponseDto>> getUserOwnProfileTemp(Principal principal);
 
-    @Operation(summary = "내가 만든 모임 조회")
-    @ResponseStatus(HttpStatus.OK)
-    @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "성공")
-    })
-    ResponseEntity<UserV2GetCreatedMeetingByUserResponseDto> getCreatedMeetingByUser(
-        Principal principal);
+	@Operation(summary = "내가 신청한 모임 조회")
+	@ResponseStatus(HttpStatus.OK)
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "성공")
+	})
+	ResponseEntity<UserV2GetAppliedMeetingByUserResponseDto> getAppliedMeetingByUser(
+		Principal principal);
 
+	@Operation(summary = "[TEMP] 내가 신청한 모임 조회")
+	@ResponseStatus(HttpStatus.OK)
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "성공")
+	})
+	ResponseEntity<TempResponseDto<UserV2GetAppliedMeetingByUserResponseDto>> getAppliedMeetingByUserTemp(
+		Principal principal);
 
+	@Operation(summary = "내가 만든 모임 조회")
+	@ResponseStatus(HttpStatus.OK)
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "성공")
+	})
+	ResponseEntity<UserV2GetCreatedMeetingByUserResponseDto> getCreatedMeetingByUser(
+		Principal principal);
 
+	@Operation(summary = "[TEMP] 내가 만든 모임 조회")
+	@ResponseStatus(HttpStatus.OK)
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "성공")
+	})
+	ResponseEntity<TempResponseDto<UserV2GetCreatedMeetingByUserResponseDto>> getCreatedMeetingByUserTemp(
+		Principal principal);
 
 }
