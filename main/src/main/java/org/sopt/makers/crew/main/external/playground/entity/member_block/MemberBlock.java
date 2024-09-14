89e5988df642379dd6f0b409ca4f3193e7e45eb3
mikekks <thdalsrb79@naver.com>
2024-09-15 02:46:09 +0900

    feat(MemberBlock): 플그의 MemberBlock 테이블 등록

---
+++ main/src/main/java/org/sopt/makers/crew/main/external/playground/entity/member_block/MemberBlock.java
@@ -0,0 +1,35 @@
+package org.sopt.makers.crew.main.external.playground.entity.member_block;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "member_block")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberBlock {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(name = "created_at")
+	private LocalDateTime createdAt;
+	@Column(name = "updated_at")
+	private LocalDateTime updatedAt;
+	@NotNull
+	@Column(name = "is_blocked")
+	private Boolean isBlocked = true;
+	@Column(name = "blocked_id")
+	private Long blockedId;
+	@Column(name = "blocker_id")
+	private Long blockerId;
+
+}
