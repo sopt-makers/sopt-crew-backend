9ee0aaf4d9c563283a351216209444e604078d2e
Mingyu Song <100754581+mikekks@users.noreply.github.com>
2024-09-16 12:35:07 +0900

    fix(Post): desc 데이터 추가 (#378)

--- main/src/main/java/org/sopt/makers/crew/main/post/v2/dto/response/PostMeetingDto.java
+++ main/src/main/java/org/sopt/makers/crew/main/post/v2/dto/response/PostMeetingDto.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+
 import org.sopt.makers.crew.main.entity.meeting.enums.MeetingCategory;
 import org.sopt.makers.crew.main.entity.meeting.vo.ImageUrlVO;
 
@@ -15,27 +16,32 @@ import org.sopt.makers.crew.main.entity.meeting.vo.ImageUrlVO;
 @Schema(name = "PostMeetingDto", description = "게시글에 대한 모임 Dto")
 public class PostMeetingDto {
 
-    @Schema(description = "모임 id", example = "1")
-    @NotNull
-    private final Integer id;
+	@Schema(description = "모임 id", example = "1")
+	@NotNull
+	private final Integer id;
 
-    @Schema(description = "모임 제목", example = "모임 제목입니다.")
-    @NotNull
-    private final String title;
+	@Schema(description = "모임 제목", example = "모임 제목입니다.")
+	@NotNull
+	private final String title;
 
-    @Schema(description = "모임 카테고리", example = "스터디")
-    @NotNull
-    private final String category;
+	@Schema(description = "모임 카테고리", example = "스터디")
+	@NotNull
+	private final String category;
 
-    @Schema(description = "모임 이미지 url", example = "[url 형식]")
-    @NotNull
-    private final List<ImageUrlVO> imageURL;
+	@Schema(description = "모임 이미지 url", example = "[url 형식]")
+	@NotNull
+	private final List<ImageUrlVO> imageURL;
 
-    @QueryProjection
-    public PostMeetingDto(Integer id, String title, MeetingCategory category, List<ImageUrlVO> imageURL) {
-        this.id = id;
-        this.title = title;
-        this.category = category.getValue();
-        this.imageURL = imageURL;
-    }
+	@Schema(description = "모임 설명", example = "모임 설명입니다.")
+	@NotNull
+	private final String desc;
+
+	@QueryProjection
+	public PostMeetingDto(Integer id, String title, MeetingCategory category, List<ImageUrlVO> imageURL, String desc) {
+		this.id = id;
+		this.title = title;
+		this.category = category.getValue();
+		this.imageURL = imageURL;
+		this.desc = desc;
+	}
 }
