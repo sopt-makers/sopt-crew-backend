7a0bd706dedb719eb3f1cc2f872cabd55b7e2bec
mikekks <thdalsrb79@naver.com>
2024-09-16 20:05:47 +0900

    del(config): 사용하지 않는 어노테이션 삭제

--- main/src/main/java/org/sopt/makers/crew/main/external/playground/entity/member_block/MemberBlockRepository.java
+++ main/src/main/java/org/sopt/makers/crew/main/external/playground/entity/member_block/MemberBlockRepository.java
@@ -1,8 +1,6 @@
 package org.sopt.makers.crew.main.external.playground.entity.member_block;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-@Qualifier("secondaryEntityManagerFactory")
 public interface MemberBlockRepository extends JpaRepository<MemberBlock, Long> {
 }
