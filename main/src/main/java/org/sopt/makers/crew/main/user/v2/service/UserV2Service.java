e540d6f0bf66b144866fece304cd081afe962e61
DongHoon Lee <dhl1924@naver.com>
2024-09-18 21:59:26 +0900

    feat(UserV2Service): id를 통해 User 객체를 가져오는 메서드 선언

--- main/src/main/java/org/sopt/makers/crew/main/user/v2/service/UserV2Service.java
+++ main/src/main/java/org/sopt/makers/crew/main/user/v2/service/UserV2Service.java
@@ -1,7 +1,9 @@
 package org.sopt.makers.crew.main.user.v2.service;
 
+import java.security.Principal;
 import java.util.List;
 
+import org.sopt.makers.crew.main.entity.user.User;
 import org.sopt.makers.crew.main.user.v2.dto.response.UserV2GetAllMeetingByUserMeetingDto;
 import org.sopt.makers.crew.main.user.v2.dto.response.UserV2GetAllMentionUserDto;
 import org.sopt.makers.crew.main.user.v2.dto.response.UserV2GetAppliedMeetingByUserResponseDto;
@@ -20,5 +22,5 @@ public interface UserV2Service {
 
 	UserV2GetCreatedMeetingByUserResponseDto getCreatedMeetingByUser(Integer userId);
 
-
+	User getUserByPrincipal(Principal principal);
 }
