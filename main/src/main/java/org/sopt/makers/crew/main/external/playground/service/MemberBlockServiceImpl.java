0943b66175958281cffd2d1b0d8626147dcc22b4
DongHoon Lee <dhl1924@naver.com>
2024-09-20 00:16:10 +0900

    refactor(MemberBlockServiceImpl): 차단자가 특정 차단된 사용자 목록에 대해 각각의 차단 상태 정보를 얻을 수 있는 메서드 구현

--- main/src/main/java/org/sopt/makers/crew/main/external/playground/service/MemberBlockServiceImpl.java
+++ main/src/main/java/org/sopt/makers/crew/main/external/playground/service/MemberBlockServiceImpl.java
@@ -4,6 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.sopt.makers.crew.main.external.playground.entity.member_block.MemberBlockRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Map;
+
 @Service
 @RequiredArgsConstructor
 public class MemberBlockServiceImpl implements MemberBlockService {
@@ -11,7 +14,7 @@ public class MemberBlockServiceImpl implements MemberBlockService {
     private final MemberBlockRepository memberBlockRepository;
 
     @Override
-    public boolean isBlockedPost(Long blockedUserOrgId, Long blockerOrgId) {
-        return memberBlockRepository.existsBlockedPost(blockedUserOrgId, blockerOrgId, true);
+    public Map<Long, Boolean> getBlockedUsers(Long blockerOrgId, List<Long> blockedUserOrgIds) {
+        return memberBlockRepository.checkBlockedUsers(blockerOrgId, blockedUserOrgIds);
     }
 }
