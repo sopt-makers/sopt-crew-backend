28cc099169d321e572fcba0715639c30196c5df1
DongHoon Lee <dhl1924@naver.com>
2024-09-19 22:22:55 +0900

    refactor(PostV2GetPostsResponseDto): DTO 객체를 PostDetailResponseDto에서 PostDetailWithBlockStatusResponseDto로 수정

--- main/src/main/java/org/sopt/makers/crew/main/post/v2/dto/response/PostV2GetPostsResponseDto.java
+++ main/src/main/java/org/sopt/makers/crew/main/post/v2/dto/response/PostV2GetPostsResponseDto.java
@@ -14,10 +14,11 @@ import org.sopt.makers.crew.main.common.pagination.dto.PageMetaDto;
 @Schema(name = "PostV2GetPostsResponseDto", description = "게시글 조회 응답 Dto")
 public class PostV2GetPostsResponseDto {
 
-	@Schema(description = "게시글 객체", example = "")
+	@Schema(description = "게시글의 기본 정보 + 댓글 썸네일 이미지 리스트 정보 + 차단된 유저의 게시물인지 아닌지 정보를 담고 있는 DTO")
 	@NotNull
-	private final List<PostDetailResponseDto> posts;
+	private final List<PostDetailWithBlockStatusResponseDto> posts;
 
+	@Schema(description = "페이지 메타 정보")
 	@NotNull
 	private final PageMetaDto meta;
 
