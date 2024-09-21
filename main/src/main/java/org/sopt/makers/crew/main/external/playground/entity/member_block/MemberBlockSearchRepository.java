d08a76a57fbeb890d646c6510d7bea3992a6e79a
DongHoon Lee <dhl1924@naver.com>
2024-09-21 19:12:58 +0900

    delete: querydsl 대신 spring data jpa를 사용하기 위해 삭제
    - spring data jpa의 메서드 네이밍이 복잡하지 않으며 성능차이도 비슷하기 때문에 코드를 줄이는 방향으로 결정

--- main/src/main/java/org/sopt/makers/crew/main/external/playground/entity/member_block/MemberBlockSearchRepository.java
+++
@@ -1,9 +0,0 @@
-package org.sopt.makers.crew.main.external.playground.entity.member_block;
-
-import java.util.List;
-
-import com.querydsl.core.Tuple;
-
-public interface MemberBlockSearchRepository {
-	List<Tuple> checkBlockedUsers(Long blockerOrgId, List<Long> userOrgIds);
-}
