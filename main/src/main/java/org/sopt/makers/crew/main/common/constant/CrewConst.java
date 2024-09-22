32f0c14bee024f00a3b2a0fd16bc1baee3a757f1
mikekks <thdalsrb79@naver.com>
2024-09-23 01:35:33 +0900

    refactor(const): final 클래스로 변경

--- main/src/main/java/org/sopt/makers/crew/main/common/constant/CrewConst.java
+++ main/src/main/java/org/sopt/makers/crew/main/common/constant/CrewConst.java
@@ -1,19 +1,22 @@
 package org.sopt.makers.crew.main.common.constant;
 
-public abstract class CrewConst {
+public final class CrewConst {
 
-    /**
-     * 매 기수 시작하기 전에 수정 필요
-     * */
-    public static final Integer ACTIVE_GENERATION = 34;
+	/**
+	 * 매 기수 시작하기 전에 수정 필요
+	 * */
+	public static final Integer ACTIVE_GENERATION = 34;
 
-    public static final String DAY_START_TIME = " 00:00:00";
-    public static final String DAY_END_TIME = " 23:59:59";
+	public static final String DAY_START_TIME = " 00:00:00";
+	public static final String DAY_END_TIME = " 23:59:59";
 
-    public static final String DAY_FORMAT = "yyyy.MM.dd";
-    public static final String DAY_TIME_FORMAT = "yyyy.MM.dd HH:mm:ss";
+	public static final String DAY_FORMAT = "yyyy.MM.dd";
+	public static final String DAY_TIME_FORMAT = "yyyy.MM.dd HH:mm:ss";
 
-    public static final String ORDER_ASC = "asc";
-    public static final String ORDER_DESC = "desc";
+	public static final String ORDER_ASC = "asc";
+	public static final String ORDER_DESC = "desc";
 
+	private CrewConst() {
+
+	}
 }
