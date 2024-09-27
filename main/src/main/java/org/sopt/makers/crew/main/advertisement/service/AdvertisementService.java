d086557c514337b47c350ae0aa927e2410682699
DongHoon Lee <dhl1924@naver.com>
2024-09-27 23:36:50 +0900

    refactor(AdvertisementService): 카테고리와 조건에 해당하는 외부광고 존재여부에 따라 광고를 반환하는 서비스 로직 구현

--- main/src/main/java/org/sopt/makers/crew/main/advertisement/service/AdvertisementService.java
+++ main/src/main/java/org/sopt/makers/crew/main/advertisement/service/AdvertisementService.java
@@ -1,13 +1,16 @@
 package org.sopt.makers.crew.main.advertisement.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.sopt.makers.crew.main.advertisement.dto.AdvertisementsGetResponseDto;
-import org.sopt.makers.crew.main.advertisement.dto.AdvertisementGetDto;
+import org.sopt.makers.crew.main.advertisement.dto.AdvertisementsGetResponseDto.AdvertisementGetDto;
 import org.sopt.makers.crew.main.common.util.Time;
 import org.sopt.makers.crew.main.entity.advertisement.Advertisement;
 import org.sopt.makers.crew.main.entity.advertisement.AdvertisementRepository;
 import org.sopt.makers.crew.main.entity.advertisement.enums.AdvertisementCategory;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,21 +20,45 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class AdvertisementService {
-	private final AdvertisementRepository advertisementRepository;
 
+	private final AdvertisementRepository advertisementRepository;
 	private final Time time;
 
 	public AdvertisementsGetResponseDto getAdvertisement(AdvertisementCategory advertisementCategory) {
-		List<Advertisement> advertisements = advertisementRepository.findTop6ByAdvertisementCategoryAndAdvertisementEndDateAfterAndAdvertisementStartDateBeforeOrderByPriority(
-			advertisementCategory, time.now(), time.now());
+		LocalDateTime now = time.now();
 
-		if (advertisements.isEmpty()) {
-			return AdvertisementsGetResponseDto.of(null);
+		int maxItems = getMaxItemsByCategory(advertisementCategory);
+		Pageable pageable = PageRequest.of(0, maxItems);
+
+		List<Advertisement> advertisements = advertisementRepository.findAdvertisementsByDateAndType(
+			advertisementCategory,
+			true,
+			now,
+			pageable);
+
+		if (!advertisements.isEmpty()) {
+			return createResponseDto(advertisements);
 		}
 
-		List<AdvertisementGetDto> advertisementDtos = advertisements.stream().map(AdvertisementGetDto::of)
-			.toList();
+		advertisements = advertisementRepository.findAdvertisementsByCategory(
+			advertisementCategory,
+			false,
+			pageable);
 
+		return createResponseDto(advertisements);
+	}
+
+	private int getMaxItemsByCategory(AdvertisementCategory category) {
+		return switch (category) {
+			case POST -> 6;
+			case MEETING -> 1;
+		};
+	}
+
+	private AdvertisementsGetResponseDto createResponseDto(List<Advertisement> advertisements) {
+		List<AdvertisementGetDto> advertisementDtos = advertisements.stream()
+			.map(AdvertisementGetDto::of)
+			.toList();
 		return AdvertisementsGetResponseDto.of(advertisementDtos);
 	}
 }
