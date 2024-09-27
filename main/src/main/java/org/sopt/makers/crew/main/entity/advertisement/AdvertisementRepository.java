9c66f50e4053f1a6f5cc95aa4c386ef9ed41777b
DongHoon Lee <dhl1924@naver.com>
2024-09-27 23:35:53 +0900

    feat(AdvertisementRepository): 조건에 맞는 광고를 가져오는 쿼리문 생성

--- main/src/main/java/org/sopt/makers/crew/main/entity/advertisement/AdvertisementRepository.java
+++ main/src/main/java/org/sopt/makers/crew/main/entity/advertisement/AdvertisementRepository.java
@@ -4,9 +4,35 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.sopt.makers.crew.main.entity.advertisement.enums.AdvertisementCategory;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AdvertisementRepository extends JpaRepository<Advertisement, Integer> {
+
 	List<Advertisement> findTop6ByAdvertisementCategoryAndAdvertisementEndDateAfterAndAdvertisementStartDateBeforeOrderByPriority(
-		AdvertisementCategory advertisementCategory, LocalDateTime now1, LocalDateTime now2);
+		AdvertisementCategory advertisementCategory, LocalDateTime now1,
+		LocalDateTime now2);    // 해당 부분 spring data jpa 코드 삭제해도 될까요?
+
+	@Query("SELECT a FROM Advertisement a " +
+		"WHERE a.isSponsoredContent = :isSponsored " +
+		"AND a.advertisementCategory = :category " +
+		"AND a.advertisementStartDate <= :now " +
+		"AND a.advertisementEndDate >= :now " +
+		"ORDER BY a.priority ASC")
+	List<Advertisement> findAdvertisementsByDateAndType(
+		@Param("category") AdvertisementCategory category,
+		@Param("isSponsored") boolean isSponsored,
+		@Param("now") LocalDateTime now,
+		Pageable pageable);
+
+	@Query("SELECT a FROM Advertisement a " +
+		"WHERE a.isSponsoredContent = :isSponsored " +
+		"AND a.advertisementCategory = :category " +
+		"ORDER BY a.priority ASC")
+	List<Advertisement> findAdvertisementsByCategory(
+		@Param("category") AdvertisementCategory category,
+		@Param("isSponsored") boolean isSponsored,
+		Pageable pageable);
 }
