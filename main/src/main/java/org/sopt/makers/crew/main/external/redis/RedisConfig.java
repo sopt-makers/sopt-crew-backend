242674de003f339799c65a647a97857de45f198a
mikekks <thdalsrb79@naver.com>
2024-11-10 18:35:30 +0900

    add(RedisConfig): redis 관련 설정 추가
    1. 직렬화, 역직렬화 관련된 설정 추가
    2. 캐시 TTL 5시간 설정

---
+++ main/src/main/java/org/sopt/makers/crew/main/external/redis/RedisConfig.java
@@ -0,0 +1,70 @@
+package org.sopt.makers.crew.main.external.redis;
+
+import java.time.Duration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+@Configuration
+@EnableCaching
+public class RedisConfig {
+
+	@Autowired
+	private RedisConnectionFactory redisConnectionFactory;
+
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate() {
+		RedisTemplate<String, Object> template = new RedisTemplate<>();
+		template.setConnectionFactory(redisConnectionFactory);
+
+		// JSON 직렬화 설정
+		ObjectMapper objectMapper = new ObjectMapper();
+		// objectMapper.registerModule(new JavaTimeModule());
+		// objectMapper.registerModule(new Hibernate6Module());
+		objectMapper.activateDefaultTyping(
+			LaissezFaireSubTypeValidator.instance,
+			ObjectMapper.DefaultTyping.NON_FINAL,
+			JsonTypeInfo.As.PROPERTY
+		);
+		GenericJackson2JsonRedisSerializer jsonRedisSerializer = new GenericJackson2JsonRedisSerializer(objectMapper);
+
+		// Key serializer 설정
+		template.setKeySerializer(new StringRedisSerializer());
+		template.setHashKeySerializer(new StringRedisSerializer());
+
+		// Value serializer 설정
+		template.setValueSerializer(jsonRedisSerializer);
+		template.setHashValueSerializer(jsonRedisSerializer);
+
+		template.afterPropertiesSet();
+		return template;
+	}
+
+	@Bean
+	public RedisCacheManager cacheManager() {
+		RedisCacheWriter redisCacheWriter = RedisCacheWriter.nonLockingRedisCacheWriter(redisConnectionFactory);
+		RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+			.serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+			.serializeValuesWith(
+				RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
+			.entryTtl(Duration.ofHours(5L)); // 캐시 TTL 설정
+
+		return new RedisCacheManager(redisCacheWriter, redisCacheConfiguration);
+	}
+}
