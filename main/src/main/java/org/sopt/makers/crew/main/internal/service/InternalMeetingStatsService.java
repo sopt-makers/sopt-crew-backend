457056aad6e7c91786c8187152b41eafb45ab7bc
DongHoon Lee <dhl1924@naver.com>
2024-11-20 14:44:42 +0900

    refactor(InternalMeetingStatsService): 비즈니스 로직 변경

--- main/src/main/java/org/sopt/makers/crew/main/internal/service/InternalMeetingStatsService.java
+++ main/src/main/java/org/sopt/makers/crew/main/internal/service/InternalMeetingStatsService.java
@@ -1,7 +1,5 @@
 package org.sopt.makers.crew.main.internal.service;
 
-import java.util.List;
-
 import org.sopt.makers.crew.main.entity.apply.ApplyRepository;
 import org.sopt.makers.crew.main.entity.apply.enums.EnApplyStatus;
 import org.sopt.makers.crew.main.entity.meeting.enums.MeetingCategory;
@@ -9,7 +7,6 @@ import org.sopt.makers.crew.main.entity.user.User;
 import org.sopt.makers.crew.main.entity.user.UserRepository;
 import org.sopt.makers.crew.main.global.exception.ErrorStatus;
 import org.sopt.makers.crew.main.global.exception.NotFoundException;
-import org.sopt.makers.crew.main.internal.dto.ApprovedStudyCountProjection;
 import org.sopt.makers.crew.main.internal.dto.ApprovedStudyCountResponseDto;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,17 +24,9 @@ public class InternalMeetingStatsService {
 		User user = userRepository.findByOrgId(orgId)
 			.orElseThrow(() -> new NotFoundException(ErrorStatus.NOT_FOUND_USER.getErrorCode()));
 
-		return fetchApprovedStudyCount(user);
-	}
+		Long approvedStudyCount = applyRepository.findApprovedStudyCountByOrgId(
+			MeetingCategory.STUDY, EnApplyStatus.APPROVE, user.getOrgId());
 
-	private ApprovedStudyCountResponseDto fetchApprovedStudyCount(User user) {
-		List<ApprovedStudyCountProjection> results = applyRepository.findApprovedStudyCountByOrgId(
-			MeetingCategory.STUDY, EnApplyStatus.APPROVE, user.getOrgId()
-		);
-
-		return results.stream()
-			.findFirst()
-			.map(ApprovedStudyCountResponseDto::fromProjection)
-			.orElse(ApprovedStudyCountResponseDto.empty(user.getOrgId()));
+		return ApprovedStudyCountResponseDto.of(user.getOrgId(), approvedStudyCount);
 	}
 }
