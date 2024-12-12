fbbab5e93e7a1a3d9457d90174d20b02f3a64b96
mikekks <thdalsrb79@naver.com>
2024-12-13 00:35:14 +0900

    fix: 잘못된 코드 원상복구

--- main/src/main/java/org/sopt/makers/crew/main/entity/apply/Apply.java
+++ main/src/main/java/org/sopt/makers/crew/main/entity/apply/Apply.java
@@ -24,9 +24,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
 @Table(name = "apply")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Apply extends BaseTimeEntity {
 
 	/**
@@ -101,13 +106,6 @@ public class Apply extends BaseTimeEntity {
 		this.status = EnApplyStatus.WAITING;
 	}
 
-	protected Apply() {
-	}
-
-	public static ApplyBuilder builder() {
-		return new ApplyBuilder();
-	}
-
 	public void updateApplyStatus(EnApplyStatus status) {
 		this.status = status;
 	}
@@ -124,168 +122,5 @@ public class Apply extends BaseTimeEntity {
 		}
 
 		return this.content;
-	}
-
-	public Integer getId() {
-		return this.id;
-	}
-
-	public EnApplyType getType() {
-		return this.type;
-	}
-
-	public Meeting getMeeting() {
-		return this.meeting;
-	}
-
-	public Integer getMeetingId() {
-		return this.meetingId;
-	}
-
-	public User getUser() {
-		return this.user;
-	}
-
-	public Integer getUserId() {
-		return this.userId;
-	}
-
-	public String getContent() {
-		return this.content;
-	}
-
-	public LocalDateTime getAppliedDate() {
-		return this.appliedDate;
-	}
-
-	public EnApplyStatus getStatus() {
-		return this.status;
-	}
-
-	public boolean equals(final Object o) {
-		if (o == this)
-			return true;
-		if (!(o instanceof Apply))
-			return false;
-		final Apply other = (Apply)o;
-		if (!other.canEqual((Object)this))
-			return false;
-		final Object this$id = this.getId();
-		final Object other$id = other.getId();
-		if (this$id == null ? other$id != null : !this$id.equals(other$id))
-			return false;
-		final Object this$type = this.getType();
-		final Object other$type = other.getType();
-		if (this$type == null ? other$type != null : !this$type.equals(other$type))
-			return false;
-		final Object this$meeting = this.getMeeting();
-		final Object other$meeting = other.getMeeting();
-		if (this$meeting == null ? other$meeting != null : !this$meeting.equals(other$meeting))
-			return false;
-		final Object this$meetingId = this.getMeetingId();
-		final Object other$meetingId = other.getMeetingId();
-		if (this$meetingId == null ? other$meetingId != null : !this$meetingId.equals(other$meetingId))
-			return false;
-		final Object this$user = this.getUser();
-		final Object other$user = other.getUser();
-		if (this$user == null ? other$user != null : !this$user.equals(other$user))
-			return false;
-		final Object this$userId = this.getUserId();
-		final Object other$userId = other.getUserId();
-		if (this$userId == null ? other$userId != null : !this$userId.equals(other$userId))
-			return false;
-		final Object this$content = this.getContent();
-		final Object other$content = other.getContent();
-		if (this$content == null ? other$content != null : !this$content.equals(other$content))
-			return false;
-		final Object this$appliedDate = this.getAppliedDate();
-		final Object other$appliedDate = other.getAppliedDate();
-		if (this$appliedDate == null ? other$appliedDate != null : !this$appliedDate.equals(other$appliedDate))
-			return false;
-		final Object this$status = this.getStatus();
-		final Object other$status = other.getStatus();
-		if (this$status == null ? other$status != null : !this$status.equals(other$status))
-			return false;
-		return true;
-	}
-
-	protected boolean canEqual(final Object other) {
-		return other instanceof Apply;
-	}
-
-	public int hashCode() {
-		final int PRIME = 59;
-		int result = 1;
-		final Object $id = this.getId();
-		result = result * PRIME + ($id == null ? 43 : $id.hashCode());
-		final Object $type = this.getType();
-		result = result * PRIME + ($type == null ? 43 : $type.hashCode());
-		final Object $meeting = this.getMeeting();
-		result = result * PRIME + ($meeting == null ? 43 : $meeting.hashCode());
-		final Object $meetingId = this.getMeetingId();
-		result = result * PRIME + ($meetingId == null ? 43 : $meetingId.hashCode());
-		final Object $user = this.getUser();
-		result = result * PRIME + ($user == null ? 43 : $user.hashCode());
-		final Object $userId = this.getUserId();
-		result = result * PRIME + ($userId == null ? 43 : $userId.hashCode());
-		final Object $content = this.getContent();
-		result = result * PRIME + ($content == null ? 43 : $content.hashCode());
-		final Object $appliedDate = this.getAppliedDate();
-		result = result * PRIME + ($appliedDate == null ? 43 : $appliedDate.hashCode());
-		final Object $status = this.getStatus();
-		result = result * PRIME + ($status == null ? 43 : $status.hashCode());
-		return result;
-	}
-
-	public static class ApplyBuilder {
-		private EnApplyType type;
-		private Meeting meeting;
-		private Integer meetingId;
-		private User user;
-		private Integer userId;
-		private String content;
-
-		ApplyBuilder() {
-		}
-
-		public ApplyBuilder type(EnApplyType type) {
-			this.type = type;
-			return this;
-		}
-
-		public ApplyBuilder meeting(Meeting meeting) {
-			this.meeting = meeting;
-			return this;
-		}
-
-		public ApplyBuilder meetingId(Integer meetingId) {
-			this.meetingId = meetingId;
-			return this;
-		}
-
-		public ApplyBuilder user(User user) {
-			this.user = user;
-			return this;
-		}
-
-		public ApplyBuilder userId(Integer userId) {
-			this.userId = userId;
-			return this;
-		}
-
-		public ApplyBuilder content(String content) {
-			this.content = content;
-			return this;
-		}
-
-		public Apply build() {
-			return new Apply(this.type, this.meeting, this.meetingId, this.user, this.userId, this.content);
-		}
-
-		public String toString() {
-			return "Apply.ApplyBuilder(type=" + this.type + ", meeting=" + this.meeting + ", meetingId="
-				+ this.meetingId + ", user=" + this.user + ", userId=" + this.userId + ", content=" + this.content
-				+ ")";
-		}
 	}
 }
