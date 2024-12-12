96118f2b5e78f044d76b343afe1c825159f50086
mikekks <thdalsrb79@naver.com>
2024-12-12 14:53:06 +0900

    chore(ImageUrlVO): 필드 final 로 변경 및 직렬화 설정

--- main/src/main/java/org/sopt/makers/crew/main/entity/meeting/vo/ImageUrlVO.java
+++ main/src/main/java/org/sopt/makers/crew/main/entity/meeting/vo/ImageUrlVO.java
@@ -1,17 +1,35 @@
 package org.sopt.makers.crew.main.entity.meeting.vo;
 
-import lombok.AccessLevel;
+import static org.sopt.makers.crew.main.global.exception.ErrorStatus.*;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode
 public class ImageUrlVO {
-	private Integer id;
-	private String url;
+	private final Integer id;
+	private final String url;
 
-	public ImageUrlVO(Integer id, String url) {
+	@JsonCreator
+	public ImageUrlVO(
+		@JsonProperty("id") Integer id,
+		@JsonProperty("url") String url) {
+
+		validateImageUrlVO(id, url);
 		this.id = id;
 		this.url = url;
+	}
+
+	private static void validateImageUrlVO(Integer id, String url) {
+		if (id == null || id < 0) {
+			throw new IllegalArgumentException(INVALID_INPUT_VALUE.getErrorCode()); // 커스텀 에러로 처리
+		}
+		if (url == null || url.isBlank()) {
+			throw new IllegalArgumentException(INVALID_INPUT_VALUE.getErrorCode());  // 커스텀 에러로 처리
+		}
 	}
 }
