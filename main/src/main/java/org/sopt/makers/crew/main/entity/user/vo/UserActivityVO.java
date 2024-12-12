76538c41b66952c0d3c9511c241f60618bb931ec
mikekks <thdalsrb79@naver.com>
2024-12-12 15:45:56 +0900

    chore(UserActivityVO): UserActivityVO final 추가

--- main/src/main/java/org/sopt/makers/crew/main/entity/user/vo/UserActivityVO.java
+++ main/src/main/java/org/sopt/makers/crew/main/entity/user/vo/UserActivityVO.java
@@ -1,28 +1,32 @@
 package org.sopt.makers.crew.main.entity.user.vo;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
-import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Getter
 @ToString
 @EqualsAndHashCode
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserActivityVO {
 
 	@Schema(description = "파트", example = "서버")
 	@NotNull
-	private String part;
+	private final String part;
 
 	@Schema(description = "기수", example = "36")
 	@NotNull
-	private int generation;
+	private final int generation;
 
-	public UserActivityVO(String part, int generation) {
+	@JsonCreator
+	public UserActivityVO(
+		@JsonProperty("part") String part,
+		@JsonProperty("generation") int generation) {
+
 		this.part = part;
 		this.generation = generation;
 	}
