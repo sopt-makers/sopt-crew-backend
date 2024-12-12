e8129bdaf70772af02190c42cb5205078e91aa75
mikekks <thdalsrb79@naver.com>
2024-12-12 14:59:44 +0900

    chore(RedisConfig): objectMapper 커스텀화하여 공통적으로 사용할 수 있게 구현

--- main/src/main/java/org/sopt/makers/crew/main/external/redis/RedisConfig.java
+++ main/src/main/java/org/sopt/makers/crew/main/external/redis/RedisConfig.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -18,10 +17,8 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @Configuration
@@ -39,49 +36,49 @@ public class RedisConfig {
 	}
 
 	@Bean
-	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+	public ObjectMapper objectMapper() {
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.registerModule(new JavaTimeModule());
+		objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS); // ISO-8601 형식
+		objectMapper.activateDefaultTyping(objectMapper.getPolymorphicTypeValidator(),
+			ObjectMapper.DefaultTyping.NON_FINAL); // 타입 정보 추가
+
+		return objectMapper;
+	}
+
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory,
+		ObjectMapper objectMapper) {
 		RedisTemplate<String, Object> template = new RedisTemplate<>();
 		template.setConnectionFactory(redisConnectionFactory);
 
-		// JSON 직렬화 설정
-		ObjectMapper objectMapper = new ObjectMapper();
-		objectMapper.findAndRegisterModules(); // 추가적으로 등록 가능한 모듈 자동 등록
-		objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS); // ISO-8601 형식 사용
-
-		GenericJackson2JsonRedisSerializer jsonRedisSerializer = new GenericJackson2JsonRedisSerializer(objectMapper);
+		StringRedisSerializer stringSerializer = new StringRedisSerializer();
+		GenericJackson2JsonRedisSerializer jsonSerializer = new GenericJackson2JsonRedisSerializer(objectMapper);
 
 		// Key serializer 설정
-		template.setKeySerializer(new StringRedisSerializer());
-		template.setHashKeySerializer(new StringRedisSerializer());
+		template.setKeySerializer(stringSerializer);
+		template.setHashKeySerializer(stringSerializer);
 
 		// Value serializer 설정
-		template.setValueSerializer(jsonRedisSerializer);
-		template.setHashValueSerializer(jsonRedisSerializer);
+		template.setValueSerializer(jsonSerializer);
+		template.setHashValueSerializer(jsonSerializer);
 
 		template.afterPropertiesSet();
 		return template;
 	}
 
-
 	@Bean
-	public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
-		// 기본 CacheWriter 설정
-		RedisCacheWriter redisCacheWriter = RedisCacheWriter.nonLockingRedisCacheWriter(redisConnectionFactory);
+	public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory, ObjectMapper objectMapper) {
+		// 동일한 직렬화 설정 공유
+		GenericJackson2JsonRedisSerializer jsonSerializer = new GenericJackson2JsonRedisSerializer(objectMapper);
 
-		// 캐시별 설정을 저장할 Map
-		Map<String, RedisCacheConfiguration> cacheConfigurations = new HashMap<>();
-
-		// 기본 TTL 설정 (특정 캐시 이름이 없는 경우 적용)
 		RedisCacheConfiguration defaultCacheConfig = RedisCacheConfiguration.defaultCacheConfig()
-			.entryTtl(Duration.ofHours(24)) // 디폴트 TTL: 24시간
+			.entryTtl(Duration.ofHours(24))
 			.serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
-			.serializeValuesWith(
-				RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+			.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jsonSerializer));
 
-		// RedisCacheManager 생성
-		return RedisCacheManager.builder(redisCacheWriter)
-			.cacheDefaults(defaultCacheConfig) // 기본 설정
-			.withInitialCacheConfigurations(cacheConfigurations) // 캐시별 설정
+		return RedisCacheManager.builder(redisConnectionFactory)
+			.cacheDefaults(defaultCacheConfig)
 			.build();
 	}
 }
