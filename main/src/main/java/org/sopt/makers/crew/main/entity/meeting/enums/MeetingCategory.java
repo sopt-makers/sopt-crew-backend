efd2a068ed7c64a8c14a812e1bbc599341f35c3a
mikekks <thdalsrb79@naver.com>
2024-12-16 13:56:50 +0900

    chore(MeetingCategory): 번개 -> 번쩍으로 변경

--- main/src/main/java/org/sopt/makers/crew/main/entity/meeting/enums/MeetingCategory.java
+++ main/src/main/java/org/sopt/makers/crew/main/entity/meeting/enums/MeetingCategory.java
@@ -6,7 +6,7 @@ import org.sopt.makers.crew.main.global.exception.BadRequestException;
 public enum MeetingCategory {
     STUDY("스터디"),
     LECTURE("강연"),
-    LIGHTNING("번개"),
+    LIGHTNING("번쩍"),
     EVENT("행사"),
     SEMINAR("세미나");
 
