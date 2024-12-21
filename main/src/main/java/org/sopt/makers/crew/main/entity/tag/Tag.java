0b676976e2f915a8cad5bc5f79f468a479abd24d
mikekks <thdalsrb79@naver.com>
2024-12-21 22:04:16 +0900

    feat(Tag): Tag 객체 정의

---
+++ main/src/main/java/org/sopt/makers/crew/main/entity/tag/Tag.java
@@ -0,0 +1,40 @@
+package org.sopt.makers.crew.main.entity.tag;
+
+import org.sopt.makers.crew.main.entity.common.BaseTimeEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+
+public class Tag extends BaseTimeEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	/**
+	 * @implSpec : 모임태그 or 번쩍태그 구분
+	 * @implNote : 모임태그일 경우, lightningId == null
+	 * @implNote : 번쩍태그일 경우, meetingId == null
+	 * */
+	@Enumerated(EnumType.STRING)
+	@Column(name = "tagType")
+	@NotNull
+	private TagType tagType;
+
+	@Column(name = "meetingId")
+	@NotNull
+	private Integer meetingId;
+
+	@Column(name = "lightningId")
+	@NotNull
+	private Integer lightningId;
+
+	@NotNull
+	private String content;
+
+}
