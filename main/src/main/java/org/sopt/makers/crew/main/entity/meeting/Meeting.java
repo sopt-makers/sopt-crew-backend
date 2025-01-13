12739a080c34dd9cd579ff1fbe02f08feca5c84c
mikekks <thdalsrb79@naver.com>
2025-01-13 23:45:53 +0900

    chore(Meeting): 최솟값 추가

--- main/src/main/java/org/sopt/makers/crew/main/entity/meeting/Meeting.java
+++ main/src/main/java/org/sopt/makers/crew/main/entity/meeting/Meeting.java
@@ -79,7 +79,7 @@ public class Meeting extends BaseTimeEntity {
 	 */
 	@Column(name = "imageURL", columnDefinition = "jsonb")
 	@Type(JsonBinaryType.class)
-	@Size(max = 1)
+	@Size(min = 1, max = 1)
 	private List<ImageUrlVO> imageURL;
 
 	/**
