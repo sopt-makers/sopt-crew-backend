226050cc7a68700063e5a4addede68170db62355
mikekks <thdalsrb79@naver.com>
2025-01-13 23:19:43 +0900

    chore(Tag): @Entity 추가

--- main/src/main/java/org/sopt/makers/crew/main/entity/tag/Tag.java
+++ main/src/main/java/org/sopt/makers/crew/main/entity/tag/Tag.java
@@ -3,13 +3,22 @@ package org.sopt.makers.crew.main.entity.tag;
 import org.sopt.makers.crew.main.entity.common.BaseTimeEntity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "tag")
 public class Tag extends BaseTimeEntity {
 
 	@Id
