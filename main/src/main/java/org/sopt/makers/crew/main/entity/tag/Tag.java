d3e17802a919c63a720db5ce5b6f83b7276b29f2
Mingyu Song <100754581+mikekks@users.noreply.github.com>
2025-01-15 11:21:20 +0900

    feat: 번쩍 관련 객체 정의 (#508)
    * feat(Lightning): Lightning 객체 정의
    
    * feat(Tag): Tag 객체 정의
    
    * feat(TagType): TagType 객체 정의
    
    * chore(Lightning): 최소 글자수 변경
    
    * chore(Meeting): ImageURL 제약조건 추가
    
    * chore(Lightning): 컬럼 이름 추가
    
    * chore(Tag): @NotNull 제거
    
    * chore(Tag): @Entity 추가
    
    * chore(Lightning): 기획수정으로 인한 엔티티 변경
    
    * chore(Meeting): 최솟값 추가
    
    * chore(Meeting): 이미지 최댓값 변경

---
+++ main/src/main/java/org/sopt/makers/crew/main/entity/tag/Tag.java
@@ -0,0 +1,47 @@
+package org.sopt.makers.crew.main.entity.tag;
+
+import org.sopt.makers.crew.main.entity.common.BaseTimeEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "tag")
+public class Tag extends BaseTimeEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	/**
+	 * @implSpec : 모임태그 or 번쩍태그 구분
+	 * @implNote : 모임태그일 경우, lightningId == null
+	 * @implNote : 번쩍태그일 경우, meetingId == null
+	 * */
+	@Enumerated(EnumType.STRING)
+	@Column(name = "tagType")
+	@NotNull
+	private TagType tagType;
+
+	@Column(name = "meetingId")
+	private Integer meetingId;
+
+	@Column(name = "lightningId")
+	private Integer lightningId;
+
+	@NotNull
+	private String content;
+
+}
