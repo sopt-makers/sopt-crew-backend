200cbc0131a049c9e60b2624773f481da3878afa
DongHoon Lee <dhl1924@naver.com>
2025-01-16 23:29:36 +0900

    refactor: 문자열 필드 추가
    - DB에 저장될 데이터는 value

--- main/src/main/java/org/sopt/makers/crew/main/entity/lightning/enums/LightningTimingType.java
+++ main/src/main/java/org/sopt/makers/crew/main/entity/lightning/enums/LightningTimingType.java
@@ -1,7 +1,25 @@
 package org.sopt.makers.crew.main.entity.lightning.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum LightningTimingType {
-	IMMEDIATE, // 당일
-	DURATION,  // 기간
-	AFTER_DISCUSSION // 협의 후 결정
+	IMMEDIATE("당일"),
+	DURATION("기간"),
+	AFTER_DISCUSSION("협의 후 결정");
+
+	private final String value;
+
+	LightningTimingType(String value) {
+		this.value = value;
+	}
+
+	public static LightningTimingType ofValue(String dbData) {
+		for (LightningTimingType timing : LightningTimingType.values()) {
+			if (timing.getValue().equals(dbData)) {
+				return timing;
+			}
+		}
+		throw new IllegalArgumentException("Invalid LightningTimingType value: " + dbData);
+	}
 }
