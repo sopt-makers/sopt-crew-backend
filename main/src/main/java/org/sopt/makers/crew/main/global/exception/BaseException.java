bb9e33bc535c31f9939aa45174c5c7f00febcffb
DongHoon Lee <dhl1924@naver.com>
2025-01-21 01:14:13 +0900

    fix: 필드 private 처리

--- main/src/main/java/org/sopt/makers/crew/main/global/exception/BaseException.java
+++ main/src/main/java/org/sopt/makers/crew/main/global/exception/BaseException.java
@@ -1,29 +1,30 @@
 package org.sopt.makers.crew.main.global.exception;
 
+import org.springframework.http.HttpStatus;
+
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.http.HttpStatus;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class BaseException extends RuntimeException {
 
-    HttpStatus httpStatus;
-    String errorCode;
+	private HttpStatus httpStatus;
+	private String errorCode;
 
-    public BaseException(HttpStatus httpStatus) {
-        super();
-        this.httpStatus = httpStatus;
-    }
+	public BaseException(HttpStatus httpStatus) {
+		super();
+		this.httpStatus = httpStatus;
+	}
 
-    public BaseException(HttpStatus httpStatus, String errorCode) {
-        super(errorCode);
-        this.httpStatus = httpStatus;
-        this.errorCode = errorCode;
-    }
+	public BaseException(HttpStatus httpStatus, String errorCode) {
+		super(errorCode);
+		this.httpStatus = httpStatus;
+		this.errorCode = errorCode;
+	}
 
-    public int getStatusCode() {
-        return this.httpStatus.value();
-    }
+	public int getStatusCode() {
+		return this.httpStatus.value();
+	}
 }
