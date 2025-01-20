157216b10bdd7530c756cfe4b2683b88d2004db2
DongHoon Lee <dhl1924@naver.com>
2025-01-21 01:09:23 +0900

    feat: message 필드 추가 및 fail 메서드 오버로딩

--- main/src/main/java/org/sopt/makers/crew/main/global/exception/ExceptionResponse.java
+++ main/src/main/java/org/sopt/makers/crew/main/global/exception/ExceptionResponse.java
@@ -1,6 +1,7 @@
 package org.sopt.makers.crew.main.global.exception;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.Builder;
 import lombok.Getter;
 
@@ -9,11 +10,19 @@ import lombok.Getter;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ExceptionResponse {
 
-  private final String errorCode;
+	private final String errorCode;
+	private final String message;
 
-  public static ExceptionResponse fail(String errorCode) {
-    return ExceptionResponse.builder()
-        .errorCode(errorCode)
-        .build();
-  }
+	public static ExceptionResponse fail(String errorCode) {
+		return ExceptionResponse.builder()
+			.errorCode(errorCode)
+			.build();
+	}
+
+	public static ExceptionResponse fail(String errorCode, String message) {
+		return ExceptionResponse.builder()
+			.errorCode(errorCode)
+			.message(message)
+			.build();
+	}
 }
