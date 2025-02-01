23103e0523cd2b422bf5c27b234b3410861305f4
DongHoon Lee <dhl1924@naver.com>
2025-02-02 00:19:11 +0900

    feat: flashId로 태그 객체를 가져오는 JPA 메서드 구현

--- main/src/main/java/org/sopt/makers/crew/main/entity/tag/TagRepository.java
+++ main/src/main/java/org/sopt/makers/crew/main/entity/tag/TagRepository.java
@@ -7,5 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface TagRepository extends JpaRepository<Tag, Integer> {
 	Optional<WelcomeMessageTypeProjection> findByFlashId(Integer flashId);
 
+	Optional<Tag> findTagByFlashId(Integer flashId);
+
 	void deleteByFlashId(Integer flashId);
 }
