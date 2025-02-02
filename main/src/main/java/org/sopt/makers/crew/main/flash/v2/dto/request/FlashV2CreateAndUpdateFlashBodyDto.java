4a5866ba305ee495ba136abbf4e9445dcdaa6fb3
DongHoon Lee <dhl1924@naver.com>
2025-02-02 14:59:22 +0900

    chore: 변경된 DTO 네이밍 적용

--- main/src/main/java/org/sopt/makers/crew/main/flash/v2/dto/request/FlashV2CreateAndUpdateFlashBodyDto.java
+++ main/src/main/java/org/sopt/makers/crew/main/flash/v2/dto/request/FlashV2CreateAndUpdateFlashBodyDto.java
@@ -6,7 +6,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 
-@Schema(name = "FlashV2CreateFlashBodyDto", description = "번쩍 모임 생성 및 수정 request body dto")
+@Schema(name = "FlashV2CreateAndUpdateFlashBodyDto", description = "번쩍 모임 생성 및 수정 request body dto")
 public record FlashV2CreateAndUpdateFlashBodyDto(
 	@Schema(description = "번쩍 모임 생성 및 수정 request body")
 	@NotNull
