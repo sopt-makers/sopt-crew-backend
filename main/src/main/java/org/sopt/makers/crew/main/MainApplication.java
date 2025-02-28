c57e8352fcfd8521a3ac1618b5aeb04f3e305343
DongHoon Lee <dhl1924@naver.com>
2025-02-28 21:56:31 +0900

    feat: @ConfigurationProperties을 통해 스프링 빈으로 등록하기 위한 @ConfigurationPropertiesScan 추가
    - 어노테이션이 적용된 클래스 패키지를 대상으로 하위 패키지에 등록된 @ConfigurationProperties 어노테이션을 스캔해서 스프링 빈으로 등록
    - 따라서 main 패키지 밑으로 스캔

--- main/src/main/java/org/sopt/makers/crew/main/MainApplication.java
+++ main/src/main/java/org/sopt/makers/crew/main/MainApplication.java
@@ -2,12 +2,14 @@ package org.sopt.makers.crew.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
 @EnableFeignClients
+@ConfigurationPropertiesScan
 public class MainApplication {
 
 	public static void main(String[] args) {
