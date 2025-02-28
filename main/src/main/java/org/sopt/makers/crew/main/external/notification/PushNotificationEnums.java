3051f491ffa4bc1bb41bcdc6757a06e6b90eb1c9
DongHoon Lee <dhl1924@naver.com>
2025-02-28 17:35:05 +0900

    feat: 번쩍 생성 시 보낼 알림메시지 생성

--- main/src/main/java/org/sopt/makers/crew/main/external/notification/PushNotificationEnums.java
+++ main/src/main/java/org/sopt/makers/crew/main/external/notification/PushNotificationEnums.java
@@ -11,13 +11,14 @@ public enum PushNotificationEnums {
 
 	PUSH_NOTIFICATION_CATEGORY("NEWS"),
 
+	// 생성 POST API(createPost, createComment, createFlash) 호출 시 알림 서버도 호출
 	NEW_POST_PUSH_NOTIFICATION_TITLE("✏️내 모임에 새로운 글이 업로드됐어요."),
 	NEW_COMMENT_PUSH_NOTIFICATION_TITLE("💬나의 모임 피드에 새로운 댓글이 달렸어요."),
+	NEW_FLASH_PUSH_NOTIFICATION_TITLE("⚡️새로운 번쩍 모임이 열렸어요!"),
 
-	NEW_COMMENT_MENTION_PUSH_NOTIFICATION_TITLE("💬%s님이 회원님을 언급했어요."),
-
+	// 별도로 분리된 mention 관련 API 호출 시 알림 서버 호출
 	NEW_POST_MENTION_PUSH_NOTIFICATION_TITLE("✏️모임 피드에서 회원님이 언급됐어요."),
-
+	NEW_COMMENT_MENTION_PUSH_NOTIFICATION_TITLE("💬%s님이 회원님을 언급했어요."),
 
 	;
 	private final String value;
