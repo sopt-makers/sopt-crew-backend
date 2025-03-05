adadad1e963169cd8b2462f10429eae075aa4ae8
DongHoon Lee <dhl1924@naver.com>
2025-03-05 18:33:56 +0900

    feat: 번쩍 생성 시 푸시 알림 비동기 전송 기능 구현
    - 번쩍 생성(FlashCreatedEvent) 발생 시 푸시 알림을 비동기로 전송하도록 구현
    - @TransactionalEventListener(TransactionPhase.AFTER_COMMIT) 적용하여 트랜잭션 커밋 후 실행되도록 처리
    - @Async("taskExecutor")를 사용하여 비동기적으로 이벤트 리스너 실행
    - 알림 서버에 보낼 PushNotificationRequestDto 생성 및 전송

---
+++ main/src/main/java/org/sopt/makers/crew/main/external/notification/event/FlashCreatedEventListener.java
@@ -0,0 +1,59 @@
+package org.sopt.makers.crew.main.external.notification.event;
+
+import static org.sopt.makers.crew.main.external.notification.PushNotificationEnums.*;
+
+import org.sopt.makers.crew.main.external.notification.PushNotificationService;
+import org.sopt.makers.crew.main.external.notification.dto.event.FlashCreatedEventDto;
+import org.sopt.makers.crew.main.external.notification.dto.request.PushNotificationRequestDto;
+import org.sopt.makers.crew.main.global.config.PushNotificationProperties;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class FlashCreatedEventListener {
+
+	private final PushNotificationService pushNotificationService;
+	private final PushNotificationProperties pushNotificationProperties;
+
+	@Async("taskExecutor")
+	@TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+	public void handleFlashCreatedEvent(FlashCreatedEventDto event) {
+		log.info("{}명 FlashCreatedEvent 수신 - meetingId: {}, title: {}", event.orgIds().size(),
+			event.meetingId(), event.pushNotificationContent());
+
+		String[] orgIdArray = event.orgIds().stream()
+			.map(String::valueOf)
+			.toArray(String[]::new);
+
+		String pushNotificationWeblink =
+			pushNotificationProperties.getPushWebUrl() + "/detail?id=" + event.meetingId();
+
+		PushNotificationRequestDto pushRequestDto = PushNotificationRequestDto.of(orgIdArray,
+			NEW_FLASH_PUSH_NOTIFICATION_TITLE.getValue(),
+			event.pushNotificationContent(),
+			PUSH_NOTIFICATION_CATEGORY.getValue(), pushNotificationWeblink);
+
+		pushNotificationService.sendPushNotification(pushRequestDto);
+
+		log.info("FlashCreatedEvent 알림 발송 완료");
+	}
+
+	@Async("taskExecutor")
+	@TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+	public void checkThread2(FlashCreatedEventDto event) {
+		log.info("두번째: {}", event.pushNotificationContent());
+	}
+
+	@Async("taskExecutor")
+	@TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+	public void checkThread3(FlashCreatedEventDto event) {
+		log.info("세번째: {}", event.pushNotificationContent());
+	}
+}
