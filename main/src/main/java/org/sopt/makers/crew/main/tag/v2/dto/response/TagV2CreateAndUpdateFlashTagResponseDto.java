02c19f99149525e21f270e80b7490ec6eadc04a6
DongHoon Lee <dhl1924@naver.com>
2025-04-20 14:02:27 +0900

    fix: 스키마 이름 수정

--- main/src/main/java/org/sopt/makers/crew/main/tag/v2/dto/response/TagV2CreateAndUpdateFlashTagResponseDto.java
+++ main/src/main/java/org/sopt/makers/crew/main/tag/v2/dto/response/TagV2CreateAndUpdateFlashTagResponseDto.java
@@ -3,7 +3,7 @@ package org.sopt.makers.crew.main.tag.v2.dto.response;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
-@Schema(name = "TagV2CreateFlashTagResponseDto", description = "번쩍 모임 태그 생성 응답 Dto")
+@Schema(name = "TagV2CreateAndUpdateFlashTagResponseDto", description = "번쩍 모임 태그 생성 및 수정 응답 Dto")
 public record TagV2CreateAndUpdateFlashTagResponseDto(
 	@Schema(description = "모임 태그 id", example = "1")
 	@NotNull
