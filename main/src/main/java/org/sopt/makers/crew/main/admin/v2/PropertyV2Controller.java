eed11b94fe05c5edd37c175f78175d7295acffe0
Khyojun <khyojun1104@gmail.com>
2025-05-08 00:51:44 +0900

    feat: homeContents를 위한 controller, vo, dto 생성

--- main/src/main/java/org/sopt/makers/crew/main/admin/v2/PropertyV2Controller.java
+++ main/src/main/java/org/sopt/makers/crew/main/admin/v2/PropertyV2Controller.java
@@ -2,8 +2,11 @@ package org.sopt.makers.crew.main.admin.v2;
 
 import java.util.List;
 
+import org.sopt.makers.crew.main.admin.v2.dto.HomePropertyResponse;
 import org.sopt.makers.crew.main.admin.v2.dto.PropertyResponse;
 import org.sopt.makers.crew.main.admin.v2.service.AdminService;
+import org.sopt.makers.crew.main.admin.v2.service.JsonPrettierService;
+import org.sopt.makers.crew.main.entity.property.Property;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,12 +20,30 @@ import lombok.RequiredArgsConstructor;
 public class PropertyV2Controller implements PropertyV2Api {
 
 	private final AdminService adminService;
+	private final JsonPrettierService jsonPrettierService;
 
 	@GetMapping("/all")
 	public ResponseEntity<List<PropertyResponse>> allProperties() {
 		return ResponseEntity.ok(adminService.findAllProperties().stream()
 			.map(PropertyResponse::from)
 			.toList());
+	}
+
+	@GetMapping("/home")
+	public ResponseEntity<HomePropertyResponse> getHomeProperty() {
+
+		List<Property> homeContentProperty = combineMainHomeProperty();
+
+		return ResponseEntity.ok(
+			HomePropertyResponse.from(jsonPrettierService.prettierHomeContent(homeContentProperty)));
+	}
+
+	private List<Property> combineMainHomeProperty() {
+		Property topContentProperty = adminService.findPropertyByKey("top");
+		Property middleContentProperty = adminService.findPropertyByKey("middle");
+		Property bottomContentProperty = adminService.findPropertyByKey("bottom");
+
+		return List.of(topContentProperty, middleContentProperty, bottomContentProperty);
 	}
 
 }
