366c461151fb37b2a2bba5e5c14979353a50a742
Khyojun <khyojun1104@gmail.com>
2025-05-12 00:41:47 +0900

    feat : key를 통한 property 조회 api 구현

--- main/src/main/java/org/sopt/makers/crew/main/admin/v2/dto/PropertyResponse.java
+++ main/src/main/java/org/sopt/makers/crew/main/admin/v2/dto/PropertyResponse.java
@@ -2,11 +2,16 @@ package org.sopt.makers.crew.main.admin.v2.dto;
 
 import java.util.Map;
 
+import org.sopt.makers.crew.main.admin.v2.service.dto.PropertyDto;
 import org.sopt.makers.crew.main.entity.property.Property;
 
 public record PropertyResponse(String key, Map<String, Object> value) {
 
 	public static PropertyResponse from(Property property) {
 		return new PropertyResponse(property.getKey(), property.getProperties());
+	}
+
+	public static PropertyResponse from(PropertyDto dto) {
+		return new PropertyResponse(dto.key(), dto.properties());
 	}
 }
