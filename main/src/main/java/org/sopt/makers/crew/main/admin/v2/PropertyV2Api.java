1d41fb8f626461a0d8977d85998c196e3bdf7792
Khyojun <khyojun1104@gmail.com>
2025-05-15 00:35:30 +0900

    feat: key에 따라 전체, 단건 응답할 수 있도록 변경

--- main/src/main/java/org/sopt/makers/crew/main/admin/v2/PropertyV2Api.java
+++ main/src/main/java/org/sopt/makers/crew/main/admin/v2/PropertyV2Api.java
@@ -1,10 +1,8 @@
 package org.sopt.makers.crew.main.admin.v2;
 
-import java.util.List;
-
 import org.sopt.makers.crew.main.admin.v2.dto.HomePropertyResponse;
-import org.sopt.makers.crew.main.admin.v2.dto.PropertyResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -14,12 +12,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "프로퍼티 조회")
 public interface PropertyV2Api {
-	@Operation(summary = "프로퍼티/조회")
-	@ApiResponses(value = {
-		@ApiResponse(responseCode = "200", description = "성공"),
-		@ApiResponse(responseCode = "401", description = "유효하지 않는 토큰입니다.", content = @Content)
-	})
-	ResponseEntity<List<PropertyResponse>> allProperties();
 
 	@Operation(summary = "프로퍼티/홈 컨텐츠 조회")
 	@ApiResponses(value = {
@@ -27,4 +19,12 @@ public interface PropertyV2Api {
 		@ApiResponse(responseCode = "401", description = "유효하지 않는 토큰입니다.", content = @Content)
 	})
 	ResponseEntity<HomePropertyResponse> getHomeProperty();
+
+	@Operation(summary = "키 값을 통한 단 건 조회(키 값이 없다면 전체 조회를 진행합니다.)")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "성공"),
+		@ApiResponse(responseCode = "401", description = "유효하지 않는 토큰입니다.", content = @Content)
+	})
+	ResponseEntity<?> getProperty(@RequestParam(required = false) String key);
+
 }
