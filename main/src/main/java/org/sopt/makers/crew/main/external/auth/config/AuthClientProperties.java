8c8e7b8acfe1ddac7c15a872b7eb682b12dc9d8d
DongHoon Lee <dhl1924@naver.com>
2025-06-04 02:05:01 +0900

    refactor: final 변수에 주입하도록 변경
    - 재할당 방지

--- main/src/main/java/org/sopt/makers/crew/main/external/auth/config/AuthClientProperties.java
+++ main/src/main/java/org/sopt/makers/crew/main/external/auth/config/AuthClientProperties.java
@@ -1,20 +1,17 @@
 package org.sopt.makers.crew.main.external.auth.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 import lombok.Getter;
-import lombok.Setter;
+import lombok.RequiredArgsConstructor;
 
 @ConfigurationProperties(prefix = "external.auth")
 @Getter
-@Setter
-@Component
+@RequiredArgsConstructor
 public class AuthClientProperties {
-	private String url;
-	private String apiKey;
-	private String serviceName;
-	private String jwk;
-	private String users;
-
+	private final String url;
+	private final String apiKey;
+	private final String serviceName;
+	private final String jwk;
+	private final String users;
 }
