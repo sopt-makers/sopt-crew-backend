47df6b9b23eabd59e82e9c06d7c26ef65548d827
DONGHOON LEE <125895298+hoonyworld@users.noreply.github.com>
2025-07-25 14:28:03 +0900

    feat: 플그에서 모임 피드 작성 POST Internal API 구현 (#701)
    * feat: 모임 피드 작성 Internal API 컨트롤러 구현
    
    * feat: internal 모임 피드 생성 dto 구현
    
    * feat: internal 모임 피드 생성 비즈니스 로직 구현
    
    * refactor: 코드레빗 리뷰 반영
    
    * refactor: 코드레빗 리뷰 반영

--- main/src/main/java/org/sopt/makers/crew/main/internal/service/InternalPostService.java
+++ main/src/main/java/org/sopt/makers/crew/main/internal/service/InternalPostService.java
@@ -1,18 +1,35 @@
 package org.sopt.makers.crew.main.internal.service;
 
-import static org.sopt.makers.crew.main.global.exception.ErrorStatus.*;
+import static org.sopt.makers.crew.main.external.notification.PushNotificationEnums.NEW_POST_PUSH_NOTIFICATION_TITLE;
+import static org.sopt.makers.crew.main.external.notification.PushNotificationEnums.PUSH_NOTIFICATION_CATEGORY;
+import static org.sopt.makers.crew.main.global.exception.ErrorStatus.FORBIDDEN_EXCEPTION;
+import static org.sopt.makers.crew.main.global.exception.ErrorStatus.INTERNAL_SERVER_ERROR;
+import static org.sopt.makers.crew.main.global.exception.ErrorStatus.NOT_FOUND_USER;
 
 import java.util.Comparator;
 import java.util.List;
 
+import org.sopt.makers.crew.main.entity.apply.Apply;
+import org.sopt.makers.crew.main.entity.apply.ApplyRepository;
+import org.sopt.makers.crew.main.entity.apply.enums.EnApplyStatus;
+import org.sopt.makers.crew.main.entity.meeting.CoLeaderRepository;
+import org.sopt.makers.crew.main.entity.meeting.Meeting;
+import org.sopt.makers.crew.main.entity.meeting.MeetingRepository;
+import org.sopt.makers.crew.main.entity.post.Post;
 import org.sopt.makers.crew.main.entity.post.PostRepository;
 import org.sopt.makers.crew.main.entity.user.User;
 import org.sopt.makers.crew.main.entity.user.UserRepository;
 import org.sopt.makers.crew.main.entity.user.vo.UserActivityVO;
+import org.sopt.makers.crew.main.external.notification.PushNotificationService;
+import org.sopt.makers.crew.main.external.notification.dto.request.PushNotificationRequestDto;
+import org.sopt.makers.crew.main.global.config.PushNotificationProperties;
+import org.sopt.makers.crew.main.global.exception.ForbiddenException;
 import org.sopt.makers.crew.main.global.exception.NotFoundException;
 import org.sopt.makers.crew.main.global.exception.ServerException;
 import org.sopt.makers.crew.main.global.pagination.dto.PageMetaDto;
 import org.sopt.makers.crew.main.global.pagination.dto.PageOptionsDto;
+import org.sopt.makers.crew.main.internal.dto.InternalPostCreateRequestDto;
+import org.sopt.makers.crew.main.internal.dto.InternalPostCreateResponseDto;
 import org.sopt.makers.crew.main.internal.dto.InternalPostGetAllResponseDto;
 import org.sopt.makers.crew.main.internal.dto.InternalPostResponseDto;
 import org.sopt.makers.crew.main.post.v2.dto.response.PostDetailWithPartBaseDto;
@@ -20,15 +37,22 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class InternalPostService {
 
 	private final PostRepository postRepository;
 	private final UserRepository userRepository;
+	private final MeetingRepository meetingRepository;
+	private final ApplyRepository applyRepository;
+	private final CoLeaderRepository coLeaderRepository;
+	private final PushNotificationService pushNotificationService;
+	private final PushNotificationProperties pushNotificationProperties;
 
 	public InternalPostGetAllResponseDto getPosts(PageOptionsDto pageOptionsDto, Integer orgId) {
 
@@ -48,6 +72,53 @@ public class InternalPostService {
 		PageMetaDto pageMetaDto = new PageMetaDto(pageOptionsDto, (int)postList.getTotalElements());
 
 		return InternalPostGetAllResponseDto.of(list, pageMetaDto);
+	}
+
+	@Transactional
+	public InternalPostCreateResponseDto createPost(InternalPostCreateRequestDto requestBody, Integer orgId) {
+		Meeting meeting = meetingRepository.findByIdOrThrow(requestBody.meetingId());
+
+		User user = userRepository.findByOrgId(orgId)
+			.orElseThrow(() -> new NotFoundException(NOT_FOUND_USER.getErrorCode()));
+
+		List<Apply> applies = applyRepository.findAllByMeetingId(meeting.getId());
+
+		boolean isInMeeting = applies.stream()
+			.anyMatch(
+				apply -> apply.getUserId().equals(user.getId()) && apply.getStatus().equals(EnApplyStatus.APPROVE));
+		boolean isMeetingCreator = meeting.getUserId().equals(user.getId());
+		boolean isCoLeader = coLeaderRepository.existsByMeetingIdAndUserId(meeting.getId(), user.getId());
+
+		if (!isInMeeting && !isMeetingCreator && !isCoLeader) {
+			throw new ForbiddenException(FORBIDDEN_EXCEPTION.getErrorCode());
+		}
+
+		Post post = Post.builder()
+			.title(requestBody.title())
+			.user(user)
+			.contents(requestBody.contents())
+			.images(requestBody.images())
+			.meeting(meeting)
+			.build();
+
+		Post savedPost = postRepository.save(post);
+
+		List<String> userIdList = applyRepository.findAllByMeetingIdAndStatus(meeting.getId(), EnApplyStatus.APPROVE)
+			.stream()
+			.map(apply -> String.valueOf(apply.getUser().getOrgId()))
+			.toList();
+
+		String[] userIds = userIdList.toArray(new String[0]);
+		String pushNotificationContent = String.format("[%s의 새 글] : \"%s\"", user.getName(), post.getTitle());
+		String pushNotificationWeblink = pushNotificationProperties.getPushWebUrl() + "/detail?id=" + meeting.getId();
+
+		PushNotificationRequestDto pushRequestDto = PushNotificationRequestDto.of(userIds,
+			NEW_POST_PUSH_NOTIFICATION_TITLE.getValue(), pushNotificationContent, PUSH_NOTIFICATION_CATEGORY.getValue(),
+			pushNotificationWeblink);
+
+		pushNotificationService.sendPushNotification(pushRequestDto);
+
+		return InternalPostCreateResponseDto.from(savedPost.getId());
 	}
 
 	private InternalPostResponseDto madeInternalResponseDto(PostDetailWithPartBaseDto dto) {
