e5f974a4b80e52f81eb8c4b18a4f4c7e1a29a9df
khyojun <38220795+khyojun@users.noreply.github.com>
2025-08-05 22:41:19 +0900

    fix: Customizer default 로 cors 받을 수 있도록 변경 (#708)
    * fix: allowCredential True로 변경하여 preFlight 실패하지 않도록 수정
    
    * fix: Customizer default 로 cors 받을 수 있도록 변경
    
    ---------
    
    Co-authored-by: khyojun <khyojun@gmail.com>

--- main/src/main/java/org/sopt/makers/crew/main/global/config/SecurityConfig.java
+++ main/src/main/java/org/sopt/makers/crew/main/global/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.sopt.makers.crew.main.global.security.filter.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -56,7 +57,7 @@ public class SecurityConfig {
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.csrf(AbstractHttpConfigurer::disable)
-			.cors(AbstractHttpConfigurer::disable)
+			.cors(Customizer.withDefaults())
 			.sessionManagement(
 				sessionManagement -> sessionManagement
 					.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
