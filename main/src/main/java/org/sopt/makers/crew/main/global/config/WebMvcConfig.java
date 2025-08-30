a956e368a86b62b84cb7671bee4e6d003d2f4675
DongHoon Lee <dhl1924@naver.com>
2025-08-30 23:16:40 +0900

    refactor: 보안 관련 설정(CORS 포함)을 SecurityConfig 한 곳에서 통합 관리 및 allowCredentials를 false로 변경

--- main/src/main/java/org/sopt/makers/crew/main/global/config/WebMvcConfig.java
+++ main/src/main/java/org/sopt/makers/crew/main/global/config/WebMvcConfig.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.sopt.makers.crew.main.global.resolver.AuthenticatedUserIdArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import lombok.RequiredArgsConstructor;
@@ -19,20 +18,5 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
 		resolvers.add(authenticatedUserIdArgumentResolver);
-	}
-
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-			.allowedOrigins(
-				"https://playground.sopt.org",
-				"http://localhost:3000",
-				"https://sopt-internal-dev.pages.dev",
-				"https://crew.api.dev.sopt.org",
-				"https://crew.api.prod.sopt.org"
-			)
-			.allowedMethods("GET", "POST", "PATCH", "DELETE", "PUT", "OPTIONS")
-			.allowedHeaders("*")
-			.allowCredentials(true);
 	}
 }
