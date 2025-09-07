d7c401a923374fb59d77d060c971cf853afc9b9f
khyojun <38220795+khyojun@users.noreply.github.com>
2025-09-07 15:38:58 +0900

    feat : crew -> pg , pg -> crew 향할 때 멘션 형식 맞출 수 있도록 구현 (#724)
    * feat: 멘션 변환용 util 클래스 작성
    
    * test : util 클래스 테스트 작성
    
    * feat: crew -> pg , pg -> crew 향할 때 멘션 형식 맞출 수 있도록 구현
    
    * fix : mention 메서드명 수정에 따른 커밋
    
    * fix: projection 받을 시점에서 변환하여 제공하여 진행 -> final 키워드 다시 붙이기 작업 진행
    
    * refactor: util class final 키워드 붙이는 작업 진행
    
    * refactor: 기본생성자 private 메서드로 막는 방식 추가

--- main/src/main/java/org/sopt/makers/crew/main/post/v2/dto/response/PostDetailWithPartBaseDto.java
+++ main/src/main/java/org/sopt/makers/crew/main/post/v2/dto/response/PostDetailWithPartBaseDto.java
@@ -1,6 +1,9 @@
 package org.sopt.makers.crew.main.post.v2.dto.response;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
+
+import org.sopt.makers.crew.main.global.util.MemberMentionConvertUtils;
 
 import com.querydsl.core.annotations.QueryProjection;
 
@@ -19,43 +22,34 @@ public class PostDetailWithPartBaseDto {
 	@Schema(description = "게시글 제목", example = "게시글 제목입니다.")
 	@NotNull
 	private final String title;
-
-	@Schema(description = "게시글 내용", example = "게시글 내용입니다.")
-	@NotNull
-	private final String contents;
-
 	@Schema(description = "게시글 생성일자", example = "2024-07-31T15:30:00")
 	@NotNull
 	private final LocalDateTime createdDate;
-
 	@Schema(description = "게시글 이미지", example = "[\"url1\", \"url2\"]")
 	@NotNull
 	private final String[] images;
-
 	@Schema(description = "게시글 작성자 객체", example = "")
 	@NotNull
 	private final PostWriterWithPartInfoDto user;
-
 	@Schema(description = "게시글 좋아요 갯수", example = "20")
 	@NotNull
 	private final int likeCount;
-
 	//* 본인이 좋아요를 눌렀는지 여부
 	@Schema(description = "게시글 좋아요 여부", example = "true")
 	@NotNull
 	private final Boolean isLiked;
-
 	@Schema(description = "게시글 조회수", example = "30")
 	@NotNull
 	private final int viewCount;
-
 	@Schema(description = "게시글 댓글 수", example = "5")
 	@NotNull
 	private final int commentCount;
-
 	@Schema(description = "게시글에 대한 모임", example = "")
 	@NotNull
 	private final PostMeetingDto meeting;
+	@Schema(description = "게시글 내용", example = "게시글 내용입니다.")
+	@NotNull
+	private final String contents;
 
 	@QueryProjection
 	public PostDetailWithPartBaseDto(Integer id, String title, String contents, LocalDateTime createdDate,
@@ -64,7 +58,7 @@ public class PostDetailWithPartBaseDto {
 		PostMeetingDto meeting) {
 		this.id = id;
 		this.title = title;
-		this.contents = contents;
+		this.contents = Objects.nonNull(contents) ? MemberMentionConvertUtils.convertMentionFormatToPg(contents) : null;
 		this.createdDate = createdDate;
 		this.images = images;
 		this.user = user;
