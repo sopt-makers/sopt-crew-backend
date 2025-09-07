34fdcf30830cbe8bd51a6adbcf81fcf5921e39a9
Khyojun <khyojun1104@gmail.com>
2025-09-07 15:14:23 +0900

    feat: 멘션 변환용 util 클래스 작성

---
+++ main/src/main/java/org/sopt/makers/crew/main/global/util/MemberMentionConvertUtils.java
@@ -0,0 +1,19 @@
+package org.sopt.makers.crew.main.global.util;
+
+public class MemberMentionConvertUtils {
+
+	public static String convertMentionFormat(String contents) {
+		if (contents == null) {
+			return null;
+		}
+		return contents.replaceAll("-~!@#@([^\\[]+)\\[(\\d+)\\]%\\^&\\*\\+", "@$1[$2]");
+	}
+
+	public static String convertMentionFormatToCrew(String contents) {
+		if (contents == null) {
+			return null;
+		}
+		return contents.replaceAll("@([^\\[]+)\\[(\\d+)\\]", "-~!@#@$1[$2]%^&*+");
+	}
+
+}
