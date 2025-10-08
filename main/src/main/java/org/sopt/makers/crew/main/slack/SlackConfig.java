5c90eaac265bc2b7362c79a0ee17433c492d667f
khyojun <khyojun1104@gmail.com>
2025-10-09 01:08:00 +0900

    feat: Slack Socket Mode 및 WebSocket 의존성 추가
    - bolt-socket-mode 라이브러리 추가
    - WebSocket API 의존성 추가 (javax.websocket, tyrus-standalone-client)
    - Slack 설정에 app-token, api-auth-token 환경변수 추가
    - SlackConfig에 MethodsClient 빈 추가
    - dev 프로파일 전용 설정 적용

--- main/src/main/java/org/sopt/makers/crew/main/slack/SlackConfig.java
+++ main/src/main/java/org/sopt/makers/crew/main/slack/SlackConfig.java
@@ -3,10 +3,14 @@ package org.sopt.makers.crew.main.slack;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+import com.slack.api.Slack;
 import com.slack.api.bolt.App;
 import com.slack.api.bolt.AppConfig;
+import com.slack.api.methods.MethodsClient;
 
+@Profile({"dev"})
 @Configuration
 public class SlackConfig {
 
@@ -18,6 +22,11 @@ public class SlackConfig {
 		return AppConfig.builder()
 			.singleTeamBotToken(botToken)
 			.build();
+	}
+
+	@Bean
+	public MethodsClient slackClient() {
+		return Slack.getInstance().methods(botToken);
 	}
 
 	@Bean
