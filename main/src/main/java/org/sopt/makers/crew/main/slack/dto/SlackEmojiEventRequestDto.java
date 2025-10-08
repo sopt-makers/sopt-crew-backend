ab7296d1c858abac7274f46db42dbe9d77a8699f
Khyojun <khyojun1104@gmail.com>
2025-10-09 02:10:15 +0900

    refactor: update dto 필요없는 필드 일부 삭제 진행

--- main/src/main/java/org/sopt/makers/crew/main/slack/dto/SlackEmojiEventRequestDto.java
+++ main/src/main/java/org/sopt/makers/crew/main/slack/dto/SlackEmojiEventRequestDto.java
@@ -2,6 +2,7 @@ package org.sopt.makers.crew.main.slack.dto;
 
 import org.sopt.makers.crew.main.slack.dto.SlackEmojiEventDto.SlackDeleteEmojiEventDto;
 import org.sopt.makers.crew.main.slack.dto.SlackEmojiEventDto.SlackUpdateEmojiEventDto;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -37,10 +38,6 @@ public class SlackEmojiEventRequestDto {
 
 		private String identifiedPwd;
 		private String originalCallEmoji;
-		private String username;
-		private String userSlackId;
-		private String team;
-		private Integer generation;
 		private String updateCallEmoji;
 
 		public SlackUpdateEmojiEventDto toDto() {
@@ -51,7 +48,6 @@ public class SlackEmojiEventRequestDto {
 		}
 	}
 
-
 	@Getter
 	@AllArgsConstructor
 	@NoArgsConstructor
@@ -60,7 +56,7 @@ public class SlackEmojiEventRequestDto {
 		private String identifiedPwd;
 		private String callEmoji;
 
-		public SlackDeleteEmojiEventDto toDto(){
+		public SlackDeleteEmojiEventDto toDto() {
 			return SlackDeleteEmojiEventDto.builder()
 				.callEmoji(callEmoji)
 				.build();
