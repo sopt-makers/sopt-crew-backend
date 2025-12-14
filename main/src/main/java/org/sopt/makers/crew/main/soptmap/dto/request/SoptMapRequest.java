0646eff89f5237b661c71d81ddaa44b9d156b0bf
DongHoon Lee <dhl1924@naver.com>
2025-12-14 17:00:51 +0900

    refactor: request, response dto 변경

---
+++ main/src/main/java/org/sopt/makers/crew/main/soptmap/dto/request/SoptMapRequest.java
@@ -0,0 +1,50 @@
+package org.sopt.makers.crew.main.soptmap.dto.request;
+
+import java.util.List;
+
+import org.sopt.makers.crew.main.entity.soptmap.MapTag;
+import org.sopt.makers.crew.main.soptmap.service.dto.CreateSoptMapDto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class SoptMapRequest {
+
+	@Getter
+	@RequiredArgsConstructor
+	public static class CreateSoptMapRequest {
+
+		@Schema(description = "장소 이름", example = "온더플랜")
+		@NotNull
+		private final String placeName;
+		@Schema(description = "주변 역 이름들", example = "['강남역', '건대역']")
+		@NotNull
+		private final List<String> stationNames;
+		@Schema(description = "한줄 소개", example = "장소 너무 좋아요")
+		@NotNull
+		private final String description;
+		@Schema(description = "장소 태그", example = "['FOOD', 'CAFE', 'ETC']")
+		@NotNull
+		private final List<MapTag> tags;
+		@Schema(description = "네이버 지도 링크", example = "https://map~~~~")
+		private final String naverLink;
+		@Schema(description = "카카오맵 링크 태그", example = "https://map~~~~")
+		private final String kakaoLink;
+
+		public CreateSoptMapDto toDto() {
+			return CreateSoptMapDto.builder()
+				.placeName(placeName)
+				.stationNames(stationNames)
+				.description(description)
+				.tags(tags)
+				.naverLink(naverLink)
+				.kakaoLink(kakaoLink)
+				.build();
+		}
+	}
+}
