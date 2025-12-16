76139aab7d5ccd35023a6f96600f27bc8211f67e
Khyojun <khyojun1104@gmail.com>
2025-12-17 00:26:36 +0900

    refactor: null 방지 코드 추가

--- main/src/main/java/org/sopt/makers/crew/main/entity/soptmap/repository/querydsl/SubwayStationQueryRepository.java
+++ main/src/main/java/org/sopt/makers/crew/main/entity/soptmap/repository/querydsl/SubwayStationQueryRepository.java
@@ -7,7 +7,7 @@ import java.util.List;
 import org.sopt.makers.crew.main.entity.soptmap.SubwayStation;
 import org.springframework.stereotype.Repository;
 
-import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.core.types.dsl.NumberTemplate;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -18,6 +18,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class SubwayStationQueryRepository {
 
+	private static final Double SIMILARITY_THRESHOLD = 0.5;
 	private final JPAQueryFactory queryFactory;
 
 	public List<SubwayStation> searchByKeyword(String keyword) {
@@ -25,14 +26,20 @@ public class SubwayStationQueryRepository {
 		NumberTemplate<Double> similarity = Expressions.numberTemplate(Double.class, "public.similarity({0}, {1})",
 			subwayStation.name, keyword);
 
-		BooleanExpression condition = subwayStation.name.likeIgnoreCase("%" + keyword + "%")
-			.or(similarity.gt(0.6));
-
 		return queryFactory.selectFrom(subwayStation)
-			.where(condition)
-			.orderBy(similarity.desc())
+			.where(condition(keyword, similarity))
+			.orderBy(keyword != null ? similarity.desc() : subwayStation.id.desc())
 			.limit(5)
 			.fetch();
+	}
+
+	private BooleanBuilder condition(String keyword, NumberTemplate<Double> similarity) {
+		BooleanBuilder builder = new BooleanBuilder();
+		if (keyword != null) {
+			builder.and(subwayStation.name.likeIgnoreCase("%" + keyword + "%")
+				.or(similarity.gt(0.6)));
+		}
+		return builder;
 	}
 
 }
