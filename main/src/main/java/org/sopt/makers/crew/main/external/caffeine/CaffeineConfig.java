7f53cb273f5f260576e64ffd127355a5ab77770e
DONGHOON LEE <125895298+move-hoon@users.noreply.github.com>
2026-01-18 01:15:01 +0900

    feat: Hybrid Cloud 환경에 맞춰 캐시 전략 이원화 (#793)
    * refactor: lambda-dev 환경에서는 Caffeine 캐시 비활성화
    
    * refactor: lambda-dev 환경에서는 Redis 캐시 활성화
    
    * feat: application-lambda-dev.yml 구현
    
    * refactor: value 형식으로 host, port 주입하도록 변경

--- main/src/main/java/org/sopt/makers/crew/main/external/caffeine/CaffeineConfig.java
+++ main/src/main/java/org/sopt/makers/crew/main/external/caffeine/CaffeineConfig.java
@@ -2,9 +2,11 @@ package org.sopt.makers.crew.main.external.caffeine;
 
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 @EnableCaching
+@Profile("!lambda-dev")
 public class CaffeineConfig {
 
 }
