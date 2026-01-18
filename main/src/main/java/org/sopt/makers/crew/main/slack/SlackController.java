37926e23fcd2084ab9750046c6ded6006e8dfec5
DongHoon Lee <dhl1924@naver.com>
2026-01-18 17:56:46 +0900

    refactor: Lambda 설정 로딩 방식을 spring.config.import로 변경
    - @Profile에 lambda-dev 추가 (CrewDatabaseConfig, PlaygroundDataSourceConfig, RealTime, Slack 관련)
    - SecretPropertiesEnvironmentPostProcessor 삭제 (spring.config.import가 Lambda에서 정상 동작 확인)
    - application-lambda-dev.yml에 spring.config.import 추가

--- main/src/main/java/org/sopt/makers/crew/main/slack/SlackController.java
+++ main/src/main/java/org/sopt/makers/crew/main/slack/SlackController.java
@@ -17,7 +17,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-@Profile({"dev"})
+@Profile({"dev", "lambda-dev"})
 @RestController
 @RequestMapping("/slack")
 @RequiredArgsConstructor
--- main/src/main/java/org/sopt/makers/crew/main/slack/SlackEventListener.java
+++ main/src/main/java/org/sopt/makers/crew/main/slack/SlackEventListener.java
@@ -12,7 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
-@Profile({"dev"})
+@Profile({"dev", "lambda-dev"})
 @RequiredArgsConstructor
 public class SlackEventListener {
 
