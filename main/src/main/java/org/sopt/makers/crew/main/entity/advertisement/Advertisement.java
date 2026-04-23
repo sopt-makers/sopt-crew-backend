6ca4d8c12a4c9122a57867440c0c4314b7065ec0
huncozyboy <leejh5838@naver.com>
2026-04-24 01:03:06 +0900

    refactor: 배너 노출 여부, 이벤트 타입, 노출 기수 컬럼 추가

--- main/src/main/java/org/sopt/makers/crew/main/entity/advertisement/Advertisement.java
+++ main/src/main/java/org/sopt/makers/crew/main/entity/advertisement/Advertisement.java
@@ -5,11 +5,11 @@ import static jakarta.persistence.GenerationType.*;
 import java.time.LocalDateTime;
 
 import org.sopt.makers.crew.main.entity.advertisement.enums.AdvertisementCategory;
+import org.sopt.makers.crew.main.entity.advertisement.enums.EventType;
+import org.sopt.makers.crew.main.entity.advertisement.enums.TargetGeneration;
 import org.sopt.makers.crew.main.entity.common.BaseTimeEntity;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
@@ -58,11 +58,28 @@ public class Advertisement extends BaseTimeEntity {
 	@NotNull
 	private boolean isSponsoredContent;
 
+	@NotNull
+	private boolean isDisplay;
+
+	/**
+	 * 배너 이벤트 타입
+	 */
+	@Enumerated(EnumType.STRING)
+	private EventType eventType;
+
+	/**
+	 * 광고 노출 기수
+	 */
+	@NotNull
+	@Enumerated(EnumType.STRING)
+	private TargetGeneration targetGeneration;
+
 	@Builder
 	private Advertisement(String advertisementDesktopImageUrl, String advertisementMobileImageUrl,
 		String advertisementLink,
 		AdvertisementCategory advertisementCategory, Long priority, LocalDateTime advertisementStartDate,
-		LocalDateTime advertisementEndDate, boolean isSponsoredContent) {
+		LocalDateTime advertisementEndDate, boolean isSponsoredContent, boolean isDisplay, EventType eventType,
+		TargetGeneration targetGeneration) {
 		this.advertisementDesktopImageUrl = advertisementDesktopImageUrl;
 		this.advertisementMobileImageUrl = advertisementMobileImageUrl;
 		this.advertisementLink = advertisementLink;
@@ -71,5 +88,12 @@ public class Advertisement extends BaseTimeEntity {
 		this.advertisementStartDate = advertisementStartDate;
 		this.advertisementEndDate = advertisementEndDate;
 		this.isSponsoredContent = isSponsoredContent;
+		this.isDisplay = isDisplay;
+		this.eventType = eventType;
+		this.targetGeneration = targetGeneration == null ? TargetGeneration.ALL : targetGeneration;
+	}
+
+	public void updateDisplay(boolean isDisplay) {
+		this.isDisplay = isDisplay;
 	}
 }
