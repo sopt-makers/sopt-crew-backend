389d49cc002ef3b81c734ead5b6a76904a23f996
huncozyboy <leejh5838@naver.com>
2026-04-24 01:37:48 +0900

    feat: 모임 상단 배너 조회 관련 MEETING_TOP ENUM 추가

--- main/src/main/java/org/sopt/makers/crew/main/entity/advertisement/enums/AdvertisementCategory.java
+++ main/src/main/java/org/sopt/makers/crew/main/entity/advertisement/enums/AdvertisementCategory.java
@@ -6,8 +6,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Getter
 public enum AdvertisementCategory {
-	POST(6),
-	MEETING(1);
+	POST(6, true),
+	MEETING(1, true),
+	MEETING_TOP(1, false);
 
 	private final int maxItems;
+	private final boolean generalAdvertisement;
 }
