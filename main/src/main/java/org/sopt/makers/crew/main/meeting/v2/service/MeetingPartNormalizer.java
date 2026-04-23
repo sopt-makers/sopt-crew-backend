5396659167ef11de7852073c44f8c1758ec38766
huncozyboy <leejh5838@naver.com>
2026-04-24 01:36:15 +0900

    feat: 파트 관련 정보 Normalizer 클래스로 분리

--- main/src/main/java/org/sopt/makers/crew/main/meeting/v2/service/MeetingPartNormalizer.java
+++ main/src/main/java/org/sopt/makers/crew/main/meeting/v2/service/MeetingPartNormalizer.java
@@ -1,5 +1,7 @@
 package org.sopt.makers.crew.main.meeting.v2.service;
 
+import java.util.Optional;
+
 import org.sopt.makers.crew.main.entity.meeting.enums.MeetingJoinablePart;
 import org.sopt.makers.crew.main.entity.user.enums.UserPart;
 import org.sopt.makers.crew.main.global.exception.BadRequestException;
@@ -20,5 +22,28 @@ public class MeetingPartNormalizer {
 		}
 
 		return part;
+	}
+
+	public Optional<MeetingJoinablePart> findJoinablePart(String part) {
+		if (part == null) {
+			return Optional.empty();
+		}
+
+		try {
+			return Optional.of(MeetingJoinablePart.valueOf(normalize(part)));
+		} catch (IllegalArgumentException exception) {
+			return Optional.empty();
+		}
+	}
+
+	public String getDisplayName(MeetingJoinablePart part) {
+		return switch (part) {
+			case PM -> "기획";
+			case DESIGN -> "디자인";
+			case IOS -> "iOS";
+			case ANDROID -> "안드로이드";
+			case SERVER -> "서버";
+			case WEB -> "웹";
+		};
 	}
 }
