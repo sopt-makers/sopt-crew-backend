3ccfa8c96bb765ea4956b2d9fae3e9a71ad4455e
khyojun <38220795+khyojun@users.noreply.github.com>
2026-04-28 23:56:29 +0900

    lambda proxy 로깅 진행 (#844)
    * feat: 람다 의존성 추가
    
    * feat: 기존 dev CI/CD 삭제
    
    * feat: 람다 deploy yml, sh 파일 추가
    
    * feat: Lambda handler 구현
    
    * feat: Lambda 관련 설정파일 추가
    
    * docs: Lambda 워크플로우 가이드 문서화
    
    * refactor: 크루 url 바뀐 경로에 맞춰서 수정
    
    * hotfix: lambda dev workflow issue resolve (#835)
    
    * Hotfix/worfklow lambda dev (#836)
    
    * hotfix: lambda dev workflow issue resolve
    
    * hotfix: lambda zip file name changed
    
    * Hotfix/worfklow lambda dev (#837)
    
    * hotfix: lambda dev workflow issue resolve
    
    * hotfix: lambda zip file name changed
    
    * hotfix: lambda gradle webflux delete & archiveFileName changed
    
    * hotfix: lambda template yaml changed (#838)
    
    * Hotfix/template yml change (#839)
    
    * hotfix: lambda template yaml changed
    
    * hotfix: lambda template yaml to servlet
    
    * hotfix: logging handler entered
    
    ---------
    
    Co-authored-by: huncozyboy <leejh5838@naver.com>
    Co-authored-by: JiHun Lee <163561527+huncozyboy@users.noreply.github.com>

---
+++ main/src/main/java/org/sopt/makers/crew/main/global/config/lambda/LambdaHandler.java
@@ -0,0 +1,66 @@
+package org.sopt.makers.crew.main.global.config.lambda;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.sopt.makers.crew.main.MainApplication;
+
+import com.amazonaws.serverless.exceptions.ContainerInitializationException;
+import com.amazonaws.serverless.proxy.internal.LambdaContainerHandler;
+import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.model.HttpApiV2ProxyRequest;
+import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class LambdaHandler implements RequestStreamHandler {
+
+	// HTTP API (v2) 형식 사용 - template의 Type: HttpApi와 일치
+	private static final SpringBootLambdaContainerHandler<HttpApiV2ProxyRequest, AwsProxyResponse> handler;
+
+	static {
+		long startTime = System.currentTimeMillis();
+
+		try {
+			log.info("Lambda Handler 초기화 시작...");
+
+			handler = SpringBootLambdaContainerHandler.getHttpApiV2ProxyHandler(MainApplication.class);
+
+			LambdaContainerHandler.getContainerConfig().addBinaryContentTypes(
+				"image/png",
+				"image/jpeg",
+				"image/gif",
+				"application/octet-stream");
+
+			long duration = System.currentTimeMillis() - startTime;
+
+			log.info("Lambda Handler 초기화 완료 (소요 시간: {}ms)", duration);
+		} catch (ContainerInitializationException e) {
+			log.error("Spring Boot 애플리케이션 초기화 실패", e);
+			throw new RuntimeException("Lambda Handler 초기화 실패 - CloudWatch Logs를 확인하세요", e);
+		}
+	}
+
+	@Override
+	public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context)
+		throws IOException {
+
+		context.getLogger().log("lambda handler entered\n");
+
+		try {
+			handler.proxyStream(inputStream, outputStream, context);
+			context.getLogger().log("lambda handler completed\n");
+		} catch (Exception e) {
+			context.getLogger().log("lambda handler failed: " + e + "\n");
+			for (StackTraceElement element : e.getStackTrace()) {
+				context.getLogger().log(element.toString() + "\n");
+			}
+			throw e;
+		}
+
+	}
+}
