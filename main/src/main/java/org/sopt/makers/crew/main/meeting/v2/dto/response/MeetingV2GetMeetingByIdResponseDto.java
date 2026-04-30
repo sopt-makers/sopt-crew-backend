3bef89b0989ee6c7a2832a69a0dd46820fef596e
huncozyboy <leejh5838@naver.com>
2026-04-30 19:01:35 +0900

    refactor: 모임 상세조회 유저 같은 파트/기수 참여 정보 분리

--- main/src/main/java/org/sopt/makers/crew/main/meeting/v2/dto/response/MeetingV2GetMeetingByIdResponseDto.java
+++ main/src/main/java/org/sopt/makers/crew/main/meeting/v2/dto/response/MeetingV2GetMeetingByIdResponseDto.java
@@ -94,9 +94,6 @@ public class MeetingV2GetMeetingByIdResponseDto {
 	@Schema(description = "참여 정보")
 	private final MeetingJoinInfo joinInfo;
 
-	@Schema(description = "조회자와 같은 파트 참여 정보")
-	private final MeetingV2ParticipatingPartInfoDto participatingPartInfo;
-
 	@Schema(description = "개설 기수", example = "36")
 	@NotNull
 	private final Integer createdGeneration;
@@ -170,7 +167,6 @@ public class MeetingV2GetMeetingByIdResponseDto {
 		Boolean isHost,
 		Boolean isApply,
 		Boolean isApproved,
-		MeetingV2ParticipatingPartInfoDto participatingPartInfo,
 		MeetingCreatorDto meetingCreatorDto,
 		List<ApplyWholeInfoDto> appliedInfo,
 		List<WelcomeMessageType> welcomeMessageTypes,
@@ -211,7 +207,6 @@ public class MeetingV2GetMeetingByIdResponseDto {
 			.isMentorNeeded(meeting.getIsMentorNeeded())
 			.canJoinOnlyActiveGeneration(meeting.getCanJoinOnlyActiveGeneration())
 			.joinInfo(meeting.getJoinInfo())
-			.participatingPartInfo(participatingPartInfo)
 			.createdGeneration(meeting.getCreatedGeneration())
 			.targetActiveGeneration(meeting.getTargetActiveGeneration())
 			.joinableParts(meeting.getJoinableParts())
