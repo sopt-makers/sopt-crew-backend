f1eee149db9304dc47375a8e4ee854333056a20f
khyojun <khyojun1104@gmail.com>
2026-05-01 23:55:40 +0900

    hotfix: redis 비활성화 및 jar 파일 미생성할 수 있도록 변경

--- main/src/main/java/org/sopt/makers/crew/main/external/caffeine/CaffeineConfig.java
+++ main/src/main/java/org/sopt/makers/crew/main/external/caffeine/CaffeineConfig.java
@@ -2,11 +2,10 @@ package org.sopt.makers.crew.main.external.caffeine;
 
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Configuration
 @EnableCaching
-@Profile("!lambda-dev")
+//@Profile("!lambda-dev")
 public class CaffeineConfig {
 
 }
