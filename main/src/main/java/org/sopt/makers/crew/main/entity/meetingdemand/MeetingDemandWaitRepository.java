04bee41e5423c8c0ed9f3d38c2bc2f55934de8f9
huncozyboy <leejh5838@naver.com>
2026-06-30 02:23:47 +0900

    feat: 모임 수요 신청 엔티티 / 레포 구현

---
+++ main/src/main/java/org/sopt/makers/crew/main/entity/meetingdemand/MeetingDemandWaitRepository.java
@@ -0,0 +1,6 @@
+package org.sopt.makers.crew.main.entity.meetingdemand;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MeetingDemandWaitRepository extends JpaRepository<MeetingDemandWait, Integer> {
+}
