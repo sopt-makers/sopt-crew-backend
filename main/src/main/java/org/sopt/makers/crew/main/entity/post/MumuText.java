4fc2bd455b6a918a31fbb6f8ab121a831cece16e
khyojun <38220795+khyojun@users.noreply.github.com>
2026-07-01 22:50:42 +0900

    fix merge conflict (#901)
    * ENG-1029
    
    * ENG-1029 무무 피드 기능 구현
    
    * ENG-1029 무무 피드 테스트 추가
    
    * ENG-1029 무무 홈 피드 응답 보강
    
    * ENG-1029 무무 문구 순환 테스트 보강
    
    * ENG-1029 무무 홈 피드 조회 조건 정리
    
    * ENG-1029 유저 관련 모임 추출 로직 분리
    
    * fix: table 엔티티 매핑 진행

--- main/src/main/java/org/sopt/makers/crew/main/entity/post/MumuText.java
+++ main/src/main/java/org/sopt/makers/crew/main/entity/post/MumuText.java
@@ -9,12 +9,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "mumu_text")
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
