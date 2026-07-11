a278d658881b5e65e427122dcac57e1eb19cd395
huncozyboy <leejh5838@naver.com>
2026-07-11 14:10:34 +0900

    test: 모임 수요 연결 해제 저장소 테스트 추가

--- main/src/test/java/org/sopt/makers/crew/main/meeting/v2/repository/MeetingRepositoryTest.java
+++ main/src/test/java/org/sopt/makers/crew/main/meeting/v2/repository/MeetingRepositoryTest.java
@@ -12,10 +12,15 @@ import org.sopt.makers.crew.main.entity.meeting.MeetingRepository;
 import org.sopt.makers.crew.main.entity.meeting.enums.MeetingCategory;
 import org.sopt.makers.crew.main.entity.meeting.enums.MeetingJoinablePart;
 import org.sopt.makers.crew.main.entity.meeting.vo.ImageUrlVO;
+import org.sopt.makers.crew.main.entity.meetingdemand.MeetingDemand;
+import org.sopt.makers.crew.main.entity.meetingdemand.MeetingDemandRepository;
+import org.sopt.makers.crew.main.entity.tag.enums.MeetingKeywordType;
 import org.sopt.makers.crew.main.entity.user.User;
 import org.sopt.makers.crew.main.entity.user.UserRepository;
 import org.sopt.makers.crew.main.global.annotation.IntegratedTest;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import jakarta.persistence.EntityManager;
 
 @IntegratedTest
 public class MeetingRepositoryTest {
@@ -24,7 +29,13 @@ public class MeetingRepositoryTest {
 	private MeetingRepository meetingRepository;
 
 	@Autowired
+	private MeetingDemandRepository meetingDemandRepository;
+
+	@Autowired
 	private UserRepository userRepository;
+
+	@Autowired
+	private EntityManager entityManager;
 
 	@Test
 	@DisplayName("정상적인 경우, 모임 저장시 성공한다.")
@@ -108,5 +119,47 @@ public class MeetingRepositoryTest {
 				"https://example.com/image1.jpg",
 				"https://example.com/image2.jpg"
 			);
+	}
+
+	@Test
+	@DisplayName("모임 수요 기반으로 개설된 모임의 수요 연결을 해제한다.")
+	void clearMeetingDemandId_success() {
+		User user = userRepository.save(User.builder()
+			.name("홍길동")
+			.orgId(1)
+			.activities(null)
+			.profileImage("image-url1")
+			.phone("010-1234-5678")
+			.build());
+		MeetingDemand meetingDemand = meetingDemandRepository.save(MeetingDemand.builder()
+			.user(user)
+			.shortIntro("러닝 모임 열어주세요")
+			.expectation("같이 꾸준히 달릴 수 있는 모임이 있으면 좋겠어요.")
+			.meetingKeywordTypes(List.of(MeetingKeywordType.EXERCISE))
+			.build());
+		Meeting meeting = meetingRepository.save(Meeting.builder()
+			.user(user)
+			.meetingDemandId(meetingDemand.getId())
+			.title("러닝 모임")
+			.category(MeetingCategory.STUDY)
+			.imageURL(List.of(new ImageUrlVO(1, "https://example.com/image1.jpg")))
+			.startDate(LocalDateTime.of(2026, 7, 1, 9, 0))
+			.endDate(LocalDateTime.of(2026, 7, 15, 18, 0))
+			.capacity(10)
+			.desc("러닝을 함께 합니다.")
+			.processDesc("매주 오프라인으로 진행합니다.")
+			.mStartDate(LocalDateTime.of(2026, 7, 16, 9, 0))
+			.mEndDate(LocalDateTime.of(2026, 8, 1, 18, 0))
+			.isMentorNeeded(false)
+			.canJoinOnlyActiveGeneration(true)
+			.createdGeneration(36)
+			.targetActiveGeneration(36)
+			.joinableParts(new MeetingJoinablePart[] {MeetingJoinablePart.SERVER})
+			.build());
+
+		meetingRepository.clearMeetingDemandId(meetingDemand.getId());
+		entityManager.clear();
+
+		Assertions.assertThat(meetingRepository.findByIdOrThrow(meeting.getId()).getMeetingDemandId()).isNull();
 	}
 }
